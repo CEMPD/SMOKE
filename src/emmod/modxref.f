54d7620db29ece1b39762266e41aab29188f2b5f
mhouyoux <>
1999-04-30 12:25:23 +0000

    Works for point speciation, temporalization, and reactivity controls

--- src/emmod/modxref.f
+++ src/emmod/modxref.f
@@ -39,12 +39,18 @@
 
         INCLUDE 'EMPRVT3.EXT'   !  private emissions string widths parameters
 
-!.........  Common per-source arrays with profile numbers
+!.........  Per-source arrays with temporal profile indices to tables
         INTEGER, ALLOCATABLE :: MDEX ( :,: ) ! monthly       profile subscript
         INTEGER, ALLOCATABLE :: WDEX ( :,: ) ! weeky         profile subscript
         INTEGER, ALLOCATABLE :: DDEX ( :,: ) ! wkday diurnal profile subscript
         INTEGER, ALLOCATABLE :: EDEX ( :,: ) ! wkend-diurnal profile subscript
  
+!.........  Per-source arrays with control packet indices to tables.  
+        INTEGER, ALLOCATABLE :: CTGIDX( :,: ) ! index for CTG data tables
+        INTEGER, ALLOCATABLE :: CTLIDX( :,: ) ! index for CONTROL data tables
+        INTEGER, ALLOCATABLE :: ALWIDX( :,: ) ! index for ALLOWABLE data tables
+        INTEGER, ALLOCATABLE :: ADDIDX( :,: ) ! index for ADD data tables
+
 !.........  Number to add to monthly profile to indicate pollutant-specific
 
         INTEGER, PARAMETER :: ADDPS = 90000
@@ -54,18 +60,26 @@
         INTEGER, PARAMETER, PUBLIC :: NXTYPES = 16
         INTEGER, PUBLIC            :: TXCNT( NXTYPES )
 
-!.........  Sorted groups of point source temporal cross-references 
+!.........  Sorted groups of cross-references
+!.........  For temporal entries, integer values are the temporal profile codes
+!.........  For speciation, CSPT* values are the speciation profile codes
+!.........  For control entries, integer values are the stored position in table
+
 !.........  Default FIPS code=0, SCC=0
 
-        INTEGER         MPRT01  !  month-of-year
-        INTEGER         WPRT01  !  day-of-week
-        INTEGER         DPRT01  !  diurnal
+        INTEGER               , ALLOCATABLE, PUBLIC :: MPRT01( : ) ! mon-of-yr
+        INTEGER               , ALLOCATABLE, PUBLIC :: WPRT01( : ) ! day-of-week
+        INTEGER               , ALLOCATABLE, PUBLIC :: DPRT01( : ) ! diurnal
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL01( : ) ! for cntrls
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT01( : ) ! spec prof
 
-!.........  FIPS code=0, SCC=left  ( TXCNT(2) )
+!.........  FIPS code=0, SCC=left  (dim: n<entries> * npol)
 
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT02( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT02( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT02( :,: )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL02( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT02( :,: )
         CHARACTER(LEN=SCCLEN3), ALLOCATABLE, PUBLIC :: CHRT02( : )
 
 !.........  FIPS code=0, SCC=all  
@@ -73,6 +87,8 @@
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT03( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT03( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT03( :,: )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL03( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT03( :,: )
         CHARACTER(LEN=SCCLEN3), ALLOCATABLE, PUBLIC :: CHRT03( : )
 
 !.........  FIPS code=state default, SCC=0  
@@ -80,6 +96,8 @@
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT04( : )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT04( : )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT04( : )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL04( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT04( :,: )
         CHARACTER(LEN=STALEN3), ALLOCATABLE, PUBLIC :: CHRT04( : )
 
 !.........  FIPS code=state default, SCC=left
@@ -87,78 +105,102 @@
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT05( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT05( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT05( :,: )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL05( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT05( :,: )
         CHARACTER(LEN=STSLEN3), ALLOCATABLE, PUBLIC :: CHRT05( : )
 
 !.........  FIPS code=state, SCC=all
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT06( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT06( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT06( :,: )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL06( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT06( :,: )
         CHARACTER(LEN=STSLEN3), ALLOCATABLE, PUBLIC :: CHRT06( : )
 
 !.........  FIPS code=all, SCC=0
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT07( : )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT07( : )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT07( : )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL07( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT07( :,: )
         CHARACTER(LEN=FIPLEN3), ALLOCATABLE, PUBLIC :: CHRT07( : )
         
 !.........  FIPS code=all, SCC=left
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT08( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT08( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT08( :,: )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL08( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT08( :,: )
         CHARACTER(LEN=FPSLEN3), ALLOCATABLE, PUBLIC :: CHRT08( : )
 
 !.........  FIPS code=all, SCC=all
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT09( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT09( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT09( :,: )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL09( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT09( :,: )
         CHARACTER(LEN=FPSLEN3), ALLOCATABLE, PUBLIC :: CHRT09( : )
 
 !.........  PLANT=non-blank, SCC=0
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT10( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT10( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT10( :,: )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL10( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT10( :,: )
         CHARACTER(LEN=FPLLEN3), ALLOCATABLE, PUBLIC :: CHRT10( : )
 
 !.........  Plant=non-blank, SCC=all
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT11( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT11( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT11( :,: )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL11( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT11( :,: )
         CHARACTER(LEN=SS0LEN3), ALLOCATABLE, PUBLIC :: CHRT11( : )
 
 !.........  CHAR1=non-blank, SCC=all
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT12( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT12( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT12( :,: )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL12( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT12( :,: )
         CHARACTER(LEN=SS1LEN3), ALLOCATABLE, PUBLIC :: CHRT12( : )
 
 !.........  CHAR2=non-blank, SCC=all
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT13( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT13( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT13( :,: )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL13( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT13( :,: )
         CHARACTER(LEN=SS2LEN3), ALLOCATABLE, PUBLIC :: CHRT13( : )
 
 !.........  CHAR3=non-blank, SCC=all
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT14( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT14( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT14( :,: )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL14( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT14( :,: )
         CHARACTER(LEN=SS3LEN3), ALLOCATABLE, PUBLIC :: CHRT14( : )
 
 !.........  CHAR4=non-blank, SCC=all
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT15( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT15( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT15( :,: )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL15( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT15( :,: )
         CHARACTER(LEN=SS4LEN3), ALLOCATABLE, PUBLIC :: CHRT15( : )
 
 !.........  CHAR5=non-blank, SCC=all
         INTEGER               , ALLOCATABLE, PUBLIC :: MPRT16( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: WPRT16( :,: )
         INTEGER               , ALLOCATABLE, PUBLIC :: DPRT16( :,: )
+        INTEGER               , ALLOCATABLE, PUBLIC :: ICTL16( :,: )
+        CHARACTER(LEN=SPNLEN3), ALLOCATABLE, PUBLIC :: CSPT16( :,: )
         CHARACTER(LEN=SS5LEN3), ALLOCATABLE, PUBLIC :: CHRT16( : )
 
 !.........  Unsorted, unprocessed cross-reference arrays
         INTEGER, ALLOCATABLE, PUBLIC:: INDXTA( : )  !  sorting index
         INTEGER, ALLOCATABLE, PUBLIC:: IFIPTA( : )  !  co/st/cy FIPS codes
-        INTEGER, ALLOCATABLE, PUBLIC:: ISPTA ( : )  !  pollutant codes  
+        INTEGER, ALLOCATABLE, PUBLIC:: ISPTA ( : )  !  pollutant index to EINAM
         INTEGER, ALLOCATABLE, PUBLIC:: MPRNA ( : )  !  monthly profile codes
         INTEGER, ALLOCATABLE, PUBLIC:: WPRNA ( : )  !  weekly profile codes
         INTEGER, ALLOCATABLE, PUBLIC:: DPRNA ( : )  !  diurnal profile codes
