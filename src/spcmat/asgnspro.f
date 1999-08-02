f3ec0b150a5eba468ccea24d086bab47086aaf55
mhouyoux <>
1999-08-02 07:02:27 +0000

    Changes to support area sources Added check to make sure that pollutant-to-pollutant conversions exists     before try to apply them Added FMTCSRC call for message writing

--- src/spcmat/asgnspro.f
+++ src/spcmat/asgnspro.f
@@ -1,6 +1,6 @@
 
-        SUBROUTINE ASGNSPRO( CATEGORY, MASSOUT, MOLEOUT, NSRC, NIPOL, 
-     &                       PNAM, EINAM, MASSMATX, MOLEMATX )
+        SUBROUTINE ASGNSPRO( MASSOUT, MOLEOUT, REPORT, NSRCIN, PNAM, 
+     &                       MASSMATX, MOLEMATX )
 
 C***********************************************************************
 C  subroutine body starts at line
@@ -46,7 +46,7 @@ C
 C***************************************************************************
 
 C...........   MODULES for public variables   
-C...........   This module contains the source ararys
+C...........   This module contains the source arrays
         USE MODSOURC
 
 C...........   This module contains the cross-reference tables
@@ -54,6 +54,9 @@ C...........   This module contains the cross-reference tables
 
 C...........   This module contains the speciation profile tables
         USE MODSPRO
+
+C.........  This module contains the information about the source category
+        USE MODINFO
 
         IMPLICIT NONE
 
@@ -70,21 +73,20 @@ C...........   EXTERNAL FUNCTIONS and their descriptions:
         EXTERNAL CRLF, ENVYN, FINDC, INDEX1
 
 C.........  SUBROUTINE ARGUMENTS
-        CHARACTER(*), INTENT    (IN) :: CATEGORY       ! source category
         LOGICAL     , INTENT    (IN) :: MASSOUT        ! true: create mass-based
         LOGICAL     , INTENT    (IN) :: MOLEOUT        ! true: create mole-based
-        INTEGER     , INTENT    (IN) :: NSRC           ! number of sources
-        INTEGER     , INTENT    (IN) :: NIPOL          ! total number of pols
+        LOGICAL     , INTENT    (IN) :: REPORT         ! true: rep defaults
+        INTEGER     , INTENT    (IN) :: NSRCIN         ! number of sources
         CHARACTER(*), INTENT    (IN) :: PNAM           ! pol name of interest
-        CHARACTER(*), INTENT    (IN) :: EINAM( NIPOL ) ! all pollutant names
-        REAL        , INTENT(IN OUT) :: MASSMATX( NSRC,* )! mass-based spec matx
-        REAL        , INTENT(IN OUT) :: MOLEMATX( NSRC,* )! mole-based spec matx
+        REAL        , INTENT(IN OUT) :: MASSMATX( NSRCIN,* )! mass spec matx
+        REAL        , INTENT(IN OUT) :: MOLEMATX( NSRCIN,* )! mole spec matx
 
 C.........  Other local variables
-        INTEGER          L2, S, V    !  counters and indices
+        INTEGER          L2, LV, S, V    !  counters and indices
 
         INTEGER          F0, F1, F2, F3, F4, F5, F6  ! tmp find indices
         INTEGER          IOS         ! i/o status
+        INTEGER          NCOUT       ! no. output source chars for mesgs
 
         REAL             CNVFAC      ! tmp pol-to-pol conversion factor
 
@@ -92,26 +94,26 @@ C.........  Other local variables
         LOGICAL, SAVE :: FIRSTIME = .TRUE.
         LOGICAL, SAVE :: REPDEFLT = .TRUE.
 
-        CHARACTER*8            FMTFIP   ! format for writing FIPS code
-        CHARACTER*300          BUFFER   ! source fields buffer
-        CHARACTER*300          MESG     ! message buffer
-        CHARACTER(LEN=FIPLEN3) CFIP     ! tmp (character) FIPS code
-        CHARACTER(LEN=FPLLEN3) CFIPPLT  ! tmp FIPS code // plant id
-        CHARACTER(LEN=FPSLEN3) CFIPSCC  ! tmp FIPS code // SCC
-        CHARACTER(LEN=FPSLEN3) CFIPSL5  ! tmp FIPS code // left SCC
-        CHARACTER(LEN=STALEN3) CSTA     ! tmp Country/state code
-        CHARACTER(LEN=STSLEN3) CSTASCC  ! tmp Country/state code // SCC
-        CHARACTER(LEN=STSLEN3) CSTASL5  ! tmp Country/state code // left SCC
-        CHARACTER(LEN=SRCLEN3) CSRC     ! tmp source chars string
-        CHARACTER(LEN=SS0LEN3) CSSC0    ! tmp FIPS // Plant // SCC
-        CHARACTER(LEN=SS1LEN3) CSSC1    ! tmp source chars through char1 // SCC
-        CHARACTER(LEN=SS2LEN3) CSSC2    ! tmp source chars through char2 // SCC
-        CHARACTER(LEN=SS3LEN3) CSSC3    ! tmp source chars through char3 // SCC
-        CHARACTER(LEN=SS4LEN3) CSSC4    ! tmp source chars through char4 // SCC
-        CHARACTER(LEN=SS5LEN3) CSSC5    ! tmp source chars through char5 // SCC
-        CHARACTER(LEN=SCCLEN3) TSCC     ! tmp 10-digit SCC
-        CHARACTER(LEN=SCLLEN3) TSCCL5   ! tmp left digits of TSCC
-        CHARACTER(LEN=SPNLEN3) SPCODE   ! tmp speciation profile code
+        CHARACTER*8               FMTFIP   ! format for writing FIPS code
+        CHARACTER*300             BUFFER   ! source fields buffer
+        CHARACTER*300             MESG     ! message buffer
+        CHARACTER(LEN=FIPLEN3)    CFIP     ! tmp (character) FIPS code
+        CHARACTER(LEN=FPLLEN3) :: CFIPPLT = ' ' ! tmp FIPS code // plant id
+        CHARACTER(LEN=FPSLEN3)    CFIPSCC  ! tmp FIPS code // SCC
+        CHARACTER(LEN=FPSLEN3)    CFIPSL   ! tmp FIPS code // left SCC
+        CHARACTER(LEN=STALEN3)    CSTA     ! tmp Country/state code
+        CHARACTER(LEN=STSLEN3)    CSTASCC  ! tmp Country/state code // SCC
+        CHARACTER(LEN=STSLEN3)    CSTASL   ! tmp Country/state code // left SCC
+        CHARACTER(LEN=SRCLEN3)    CSRC     ! tmp source chars string
+        CHARACTER(LEN=SS0LEN3) :: CSSC0 = ' '! tmp FIPS // Plant // SCC
+        CHARACTER(LEN=SS1LEN3) :: CSSC1 = ' '! tmp src chars -> char1 // SCC
+        CHARACTER(LEN=SS2LEN3) :: CSSC2 = ' '! tmp src chars -> char2 // SCC
+        CHARACTER(LEN=SS3LEN3) :: CSSC3 = ' '! tmp src chars -> char3 // SCC
+        CHARACTER(LEN=SS4LEN3) :: CSSC4 = ' '! tmp src chars -> char4 // SCC
+        CHARACTER(LEN=SS5LEN3) :: CSSC5 = ' '! tmp src chars -> char5 // SCC
+        CHARACTER(LEN=SCCLEN3)    TSCC     ! tmp 10-digit SCC
+        CHARACTER(LEN=SCCLEN3)    TSCCL    ! tmp left digits of TSCC
+        CHARACTER(LEN=SPNLEN3)    SPCODE   ! tmp speciation profile code
 
         CHARACTER*16 :: PROGNAME = 'ASGNSPRO' ! program name
 
@@ -134,30 +136,58 @@ C.............  Set up format for creating character FIPS code for non-point
 
         ENDIF
 
-C.........  Find index in complete list of pollutants
-        V = INDEX1( PNAM, NIPOL, EINAM )
+C.........  Set number of output fields for FMTCSRC to use
+        SELECT CASE ( CATEGORY )
+        CASE ( 'AREA' ) 
+            NCOUT = 1
+        CASE ( 'MOBILE' )
+            NCOUT = NCHARS
+        CASE ( 'POINT' )
+            NCOUT = NCHARS
+        END SELECT
 
-        DO S = 1, NSRC
+C.........  Initialize matrices to 0.
+        IF( MASSOUT ) THEN
+            MASSMATX( :,1:MXSPEC ) = 0.    ! array
+        END IF
+
+        IF( MOLEOUT ) THEN
+            MOLEMATX( :,1:MXSPEC ) = 0.    ! array
+        END IF
+
+C.........  Find index in complete list of pollutants and set length of name
+        V  = INDEX1( PNAM, NIPOL, EINAM ) 
+        LV = LEN_TRIM( EINAM( V ) )
+
+        DO S = 1, NSRCIN
 
 C.............  Create selection 
             SELECT CASE ( CATEGORY )
 
             CASE ( 'AREA' ) 
-                WRITE( CFIP, FMTFIP ) IFIP( S )
 
+                CSRC    = CSOURC( S )
+                CFIP    = CSRC( 1:FIPLEN3 )
                 CSTA    = CFIP( 1:STALEN3 )
                 TSCC    = CSCC( S )
-                TSCCL5  = TSCC( 1:SCLLEN3 )
+                TSCCL   = TSCC( 1:LSCCEND )
                 CFIPSCC = CFIP // TSCC
-                CFIPSL5 = CFIP // TSCCL5
+                CFIPSL  = CFIP // TSCCL 
                 CSTASCC = CSTA // TSCC
-                CSTASL5 = CSTA // TSCCL5
+                CSTASL  = CSTA // TSCCL 
 
             CASE ( 'MOBILE' )
+
+                CSRC    = CSOURC( S )
+                CFIP    = CSRC( 1:FIPLEN3 )
+                CSTA    = CFIP( 1:STALEN3 )
+c note: add here when ready for mobile
 
             CASE ( 'POINT' )
 
                 CSRC    = CSOURC( S )
+                CFIP    = CSRC( 1:FIPLEN3 )
+                CSTA    = CSRC( 1:STALEN3 )
                 TSCC    = CSCC( S )
                 CSSC5   = CSRC( 1:PTENDL3( 7 ) ) // TSCC
                 CSSC4   = CSRC( 1:PTENDL3( 6 ) ) // TSCC
@@ -166,13 +196,11 @@ C.............  Create selection
                 CSSC1   = CSRC( 1:PTENDL3( 3 ) ) // TSCC
                 CSSC0   = CSRC( 1:PTENDL3( 2 ) ) // TSCC
                 CFIPPLT = CSRC( 1:PTENDL3( 2 ) )
-                CFIP    = CSRC( 1:FIPLEN3 )
-                CSTA    = CSRC( 1:STALEN3 )
-                TSCCL5  = TSCC( 1:SCLLEN3 )
+                TSCCL   = TSCC( 1:LSCCEND )
                 CFIPSCC = CFIP // TSCC
-                CFIPSL5 = CFIP // TSCCL5
+                CFIPSL  = CFIP // TSCCL
                 CSTASCC = CSTA // TSCC
-                CSTASL5 = CSTA // TSCCL5
+                CSTASL  = CSTA // TSCCL
                     
             CASE DEFAULT
 
@@ -180,30 +208,41 @@ C.............  Create selection
 
 C.........................................................................
 C.............  Initialize speciation matrices using pollutant-to-pollutant
-C               conversion factors 
+C               conversion factors, if they exist
 C.........................................................................
 
-C.............  Try for pollutant-specific FIPS code & SCC match; then
-C                       pollutant-specific Cy/st code & SCC match; then
-C                       pollutant-specific SCC match
+C.............  Screen for pollutant-to-pollutant conversion factors by checking
+C               if they have been allocated
+            IF( ALLOCATED( CNVRT03 ) ) THEN
 
-            F3 = FINDC( CFIPSCC, NCNV3, CNVRT03 ) 
-            F2 = FINDC( CSTASCC, NCNV2, CNVRT02 ) 
-            F1 = FINDC( TSCC   , NCNV1, CNVRT01 ) 
+C.................  Try for pollutant-specific FIPS code & SCC match; then
+C                           pollutant-specific Cy/st code & SCC match; then
+C                           pollutant-specific SCC match
 
-            IF( F3 .GT. 0 .AND. CNVFC03(F3,V) .NE. AMISS3 ) THEN
-                CNVFAC = CNVFC03( F3,V )
+                F3 = FINDC( CFIPSCC, NCNV3, CNVRT03 ) 
+                F2 = FINDC( CSTASCC, NCNV2, CNVRT02 ) 
+                F1 = FINDC( TSCC   , NCNV1, CNVRT01 ) 
 
-            ELSE IF( F2 .GT. 0 .AND. CNVFC02(F2,V) .NE. AMISS3 ) THEN
-                CNVFAC = CNVFC02( F2,V )
+        	IF( F3 .GT. 0 .AND. CNVFC03(F3,V) .NE. AMISS3 ) THEN
+                    CNVFAC = CNVFC03( F3,V )
 
-            ELSE IF( F1 .GT. 0 .AND. CNVFC01(F1,V) .NE. AMISS3 ) THEN
-                CNVFAC = CNVFC01( F1,V )
+        	ELSE IF( F2 .GT. 0 .AND. CNVFC02(F2,V) .NE. AMISS3 ) THEN
+                    CNVFAC = CNVFC02( F2,V )
 
-C.............  CNVFC00( V ) will equal 1.0 if it has not been set, so there
-C               is no need for error checking
+        	ELSE IF( F1 .GT. 0 .AND. CNVFC01(F1,V) .NE. AMISS3 ) THEN
+                    CNVFAC = CNVFC01( F1,V )
+
+C.................  CNVFC00( V ) will equal 1.0 if it has not been set, so 
+C                   there is no need for error checking
+        	ELSE
+                    CNVFAC = CNVFC00( V )
+
+        	END IF
+
+C.............  If they don't exist, simply set the conversion factor to one
             ELSE
-                CNVFAC = CNVFC00( V )
+
+                CNVFAC = 1.
 
             END IF
 
@@ -276,11 +315,11 @@ C                       pollutant-specific SCC match; then
 C                       pollutant-specific left SCC match
 
             F5 = FINDC( CFIPSCC, TXCNT( 9 ), CHRT09 ) 
-            F4 = FINDC( CFIPSL5, TXCNT( 8 ), CHRT08 ) 
+            F4 = FINDC( CFIPSL , TXCNT( 8 ), CHRT08 ) 
             F3 = FINDC( CSTASCC, TXCNT( 6 ), CHRT06 ) 
-            F2 = FINDC( CSTASL5, TXCNT( 5 ), CHRT05 ) 
+            F2 = FINDC( CSTASL , TXCNT( 5 ), CHRT05 ) 
             F1 = FINDC( TSCC   , TXCNT( 3 ), CHRT03 ) 
-            F0 = FINDC( TSCCL5 , TXCNT( 2 ), CHRT02 ) 
+            F0 = FINDC( TSCCL  , TXCNT( 2 ), CHRT02 ) 
 
             IF( F5 .GT. 0 .AND. CSPT09(F5,V) .NE. EMCMISS3 ) THEN
                 SPCODE = CSPT09( F5,V ) 
@@ -332,32 +371,48 @@ C.............  Try for any country/state code match (not, pol-specific)
                 CYCLE                       !  to end of sources-loop
             END IF
 
-            IF( CSPT01( V ) .NE. EMCMISS3 .AND. REPDEFLT ) THEN
+            IF( CSPT01( V ) .NE. EMCMISS3 .AND. 
+     &          REPDEFLT .AND. REPORT           ) THEN
                 SPCODE = CSPT01( V )
                     
-                CALL FMTCSRC( CSRC, 7, BUFFER, L2 )
+                CALL FMTCSRC( CSRC, NCOUT, BUFFER, L2 )
 
-                WRITE( MESG,94010 )
-     &                 'NOTE: Using default speciation profile "' //
-     &                 SPCODE // '" for:'//
-     &                 CRLF() // BLANK10 // BUFFER( 1:L2 ) //
-     &                 CRLF() // BLANK10 // 
-     &                 'SCC: ' // TSCC // ' POL: ' // EINAM( V )
-                CALL M3MESG( MESG )
+C.................  For default speciation profile, make sure that it has been
+C                   defined for the current pollutant
+                IF( SPCODE .EQ. ' ' ) THEN
 
-                CALL SETSOURCE_SMATS
+                    EFLAG = .TRUE.
+                    MESG = 'ERROR: Default speciation profile not ' //
+     &                     'defined for POL: '// EINAM(V)(1:LV)// ','//
+     &                     CRLF() // BLANK10 // 'but needed for:'//
+     &                     CRLF() // BLANK10 // BUFFER( 1:L2 ) //
+     &                     CRLF() // BLANK10 // 
+     &                     'SCC: ' // TSCC
+                    CALL M3MESG( MESG )
+
+C.................  It profile defined, try to apply it
+                ELSE
+
+                    MESG = 'NOTE: Using default speciation profile "' //
+     &                     SPCODE // '" for:'//
+     &                     CRLF() // BLANK10 // BUFFER( 1:L2 ) //
+     &                     CRLF() // BLANK10 // 
+     &                     'SCC: ' // TSCC // ' POL: ' // EINAM( V )
+                    CALL M3MESG( MESG )
+
+                    CALL SETSOURCE_SMATS
+
+                 END IF
 
             ELSEIF( CSPT01( V ) .NE. EMCMISS3 ) THEN
                 SPCODE = CSPT01( V )
                 CALL SETSOURCE_SMATS
 
             ELSE
-                CALL FMTCSRC( CSRC, 7, BUFFER, L2 )
+                CALL FMTCSRC( CSRC, NCOUT, BUFFER, L2 )
 
                 EFLAG = .TRUE.
-
-                WRITE( MESG,94010 )
-     &                 'ERROR: No speciation cross-reference ' //
+                MESG = 'ERROR: No speciation cross-reference ' //
      &                 'available (and no default) for:' //
      &                 CRLF() // BLANK10 // BUFFER( 1:L2 ) //
      &                 CRLF() // BLANK10 // 
@@ -407,11 +462,11 @@ C----------------------------------------------------------------------
 
             IF( K .EQ. 0 ) THEN
 
-                CALL FMTCSRC( CSRC, 7, BUFFER, L2 )
+                CALL FMTCSRC( CSRC, NCOUT, BUFFER, L2 )
 
                 EFLAG = .TRUE.
                 MESG = 'ERROR: Speciation profile "' // SPCODE // 
-     &                 '" is not in profiles, but was assigned' //
+     &                 '" is not in profiles, but it was assigned' //
      &                 CRLF() // BLANK10 // 'to source:' //
      &                 CRLF() // BLANK10 // BUFFER( 1:L2 ) //
      &                 CRLF() // BLANK10 // 
@@ -431,11 +486,11 @@ C.................  Get indices to full speciation table
                     J = IDXSSPEC( K,N )
 
                     IF( MASSOUT ) THEN
-                        MASSMATX( S,J )= TON2GM * CNVFAC * MASSFACT( I )
+                        MASSMATX( S,J )= CNVFAC * MASSFACT( I )
                     END IF
 
                     IF( MOLEOUT ) THEN
-                        MOLEMATX( S,J )= TON2GM * CNVFAC * MOLEFACT( I )
+                        MOLEMATX( S,J )= CNVFAC * MOLEFACT( I )
                     END IF
 
                 END DO 
