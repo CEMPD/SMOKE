fe309d846214c68f17b284c6c88894cb46ddddef
vukovich <>
1999-11-23 13:44:03 +0000

    Initial revision

---
+++ src/biog/cldaten_calc.f
@@ -0,0 +1,305 @@
+
+      SUBROUTINE CLDATEN_CALC( JDATE, JTIME, NX, NY, LAT ,LON ,
+     &                         CLOUDTYPE ,CLDATN )
+
+C***********************************************************************
+C  program body starts at line 138
+C
+C  DESCRIPTION:
+C       Computes gridded cloud attenuation factor CLDATN for SMOKE BEIS2
+C       Based on the cloud type string in CLDTYPE, reads cloud cover data
+C       from Met-Chem Interface Processor for either the KUO (ANTHES-KUO)
+C       cloud recalculation, or the McHenry-Kain (KAIN-) cloud recalculation.
+C       If CLDTYPE= 'No deep convection param', it is assumed that MM5 ran 
+C       a simulation without a deep convection parameterization. This is 
+C       typically done  only for urban-scale problems where the 
+C       grid-resolution is 4km or less. 
+C
+C  PRECONDITIONS REQUIRED:
+C
+C       NOT TESTED FULLY YET!  Use BG_CLOUD_TYPE set to 1 or 5 at this
+C       time. 
+C
+C  SUBROUTINES AND FUNCTIONS CALLED:
+C
+C  REVISION  HISTORY:
+C
+C      11/99: by Jeff Vukovich taken from v4.4 of SMOKE prototype
+C
+C***********************************************************************
+C
+C Project Title: Sparse Matrix Operator Kernel Emissions (SMOKE) Modeling
+C                System
+C File: @(#)$Id$
+C
+C COPYRIGHT (C) 1999, MCNC--North Carolina Supercomputing Center
+C All Rights Reserved
+C
+C See file COPYRIGHT for conditions of use.
+C
+C Environmental Programs Group
+C MCNC--North Carolina Supercomputing Center
+C P.O. Box 12889
+C Research Triangle Park, NC  27709-2889
+C
+C env_progs@mcnc.org
+C
+C Pathname: $Source$
+C Last updated: $Date$ 
+C
+C***********************************************************************
+
+      IMPLICIT NONE
+
+C...........   INCLUDES:
+
+        INCLUDE 'PARMS3.EXT'      ! I/O API constants
+        INCLUDE 'FDESC3.EXT'      ! I/O API file description data structure
+        INCLUDE 'IODECL3.EXT'     ! I/O API function declarations
+        INCLUDE 'CONST3.EXT'      ! I/O API constants
+        INCLUDE 'EMCNST3.EXT'     ! emissions constants
+        INCLUDE 'BIODIMS3.EXT'    ! biogenic-related constants
+
+
+C...........   ARGUMENTS and their descriptions:
+
+        INTEGER, INTENT (IN)  :: JDATE   ! current simulation date (YYYYDDD)
+        INTEGER, INTENT (IN)  :: JTIME   ! current simulation time (HHMMSS)
+        INTEGER, INTENT (IN)  :: NX      ! no. cols
+        INTEGER, INTENT (IN)  :: NY      ! no. rows
+        REAL, INTENT (IN)  ::  LAT  ( NX, NY )  !  lat (deg) -90 <= LAT <= 90
+        REAL, INTENT (IN)  ::  LON  ( NX, NY )  !  lon (deg) -180 <= LON <= 180
+        CHARACTER*16, INTENT(IN) :: CLOUDTYPE
+
+        REAL, INTENT (OUT)  ::  CLDATN( NX, NY ) !  cloud attenuation factor 
+      
+C...........   PARAMETERS and their descriptions:
+C
+      REAL THIN
+      PARAMETER(THIN = 200.)  ! thin cld cover <= 200m thick
+      REAL            SIGA, SDEC, D100, D60, ROTDAY
+
+      PARAMETER (
+     &            SIGA = 279.9348  ,
+     &            SDEC = 0.39784984 , ! SIN (23^26'37.8") the declination angle
+     &            D100 = 1.0 /100.0,
+     &             D60 = 1.0 / 60.0,
+     &          ROTDAY = 360.0 / 365.242 )   ! rotation per day
+
+
+C.......   Variables for zenith angle calculation:
+
+      REAL            DADM, SNDAY, CSDAY, SNDA2, CSDA2
+      REAL            AMM, DESIN, DECOS
+      REAL            SIGMA, CZ
+      REAL            H0, H1
+C
+C -- empirical factors for attenuation related to height of the
+C    cloud base:  position 1: for obscured sky (not used here);
+C                 position 2: for cld bases < 1000m
+C                 position 3: for cld bases < 3000m
+C                 position 4: for cld bases < 7000m
+C                 position 5: for cld bases above 7000m
+C
+      REAL            AA( 5 ), BB( 5 )
+
+      DATA            AA / 0.165, 0.18,  0.365,  0.47,  0.89 /
+      DATA            BB / 0.005, 0.02, -0.015, -0.01, -0.04 /
+
+C
+C...........   LOCAL VARIABLES and their descriptions:
+C
+      REAL, ALLOCATABLE :: CLDT ( :, : )  ! ave. cloud top in meters
+      REAL, ALLOCATABLE :: CLDB ( :, : )  ! ave. cloud bottom in meters
+      REAL, ALLOCATABLE :: CFRAC( :, : )  ! fractional cloud coverage
+      REAL            CBOT, CTOP
+      REAL            FRAC, CTHK
+      REAL            TTYPE 
+      REAL            T
+
+      INTEGER         R,C,ROW,COL, I, NCLAY
+
+      LOGICAL         LTHIN
+      LOGICAL         FIRSTIME
+      DATA            FIRSTIME / .TRUE. /
+      SAVE            FIRSTIME
+
+      CHARACTER*5     CLTYPE
+      CHARACTER*16    CLDTVAR   ! Name of input variable
+      CHARACTER*16    CLDBVAR   ! Name of input variable
+      CHARACTER*16    CFRACVAR  ! Name of input variable
+      SAVE            CLTYPE, CLDTVAR, CLDBVAR, CFRACVAR
+
+      CHARACTER*16    PNAME
+      DATA            PNAME / 'CLDATEN_CALC' /
+C
+C***********************************************************************
+C   begin body of program CLDATEN_CALC
+
+      IF ( FIRSTIME ) THEN
+
+          FIRSTIME = .FALSE.
+          IF(CLOUDTYPE(1:5).EQ.'ANTHE') THEN
+            CALL M3MESG( 'Using Kuo-package cld data for CLDATEN' )
+            CLDTVAR = 'CLDT_KUO'
+            CLDBVAR = 'CLDB_KUO'
+            CFRACVAR= 'CFRAC_KUO'
+
+          ELSE IF(CLOUDTYPE(1:5).EQ.'KAIN-') THEN
+            CALL M3MESG( 'Using KF-package cld data for CLDATEN' )
+            CLDTVAR = 'CLDT_KF'
+            CLDBVAR = 'CLDB_KF'
+            CFRACVAR= 'CFRAC_KF'
+
+          ELSE IF(CLOUDTYPE(1:5).EQ.'NO CU') THEN
+            CALL M3MESG( 'Using explicit w/out deep cu cld data 
+     &                    for CLDATEN' )
+            CLDTVAR = 'CLDT_URB'
+            CLDBVAR = 'CLDB_URB'
+            CFRACVAR = 'CFRAC_URB'
+
+          ELSE
+            CALL M3EXIT( PNAME, JDATE, JTIME,
+     &                   'Unsupported or unknown cloud type', 2 )
+
+          END IF
+          CLTYPE(1:5) = CLOUDTYPE(1:5)
+
+      END IF                  !  if firstime
+C
+C --- first compute (from spherical geometry) factors needed to 
+C     get the cosine of the zenith angle dependent on time of day.
+C
+C
+      DADM  = ROTDAY * FLOAT( MOD( JDATE, 1000 ) - 1 )
+      SNDAY = SIN( PI180 * DADM )
+      CSDAY = COS( PI180 * DADM )
+      SNDA2 = 2.0 * SNDAY * CSDAY
+      CSDA2 = CSDAY * CSDAY - SNDAY * SNDAY
+      SIGMA  =   SIGA + DADM
+     &             + 1.914827 * SNDAY  - 0.079525 * CSDAY
+     &             + 0.019938 * SNDA2 - 0.001620 * CSDA2
+      DESIN  =  SDEC * SIN( PI180 * SIGMA )
+      DECOS  =  SQRT( 1.0 - DESIN * DESIN )
+
+      AMM   =  12.0 + 0.123570 * SNDAY - 0.004289 * CSDAY
+     &                + 0.153809 * SNDA2 + 0.060783 * CSDA2
+      H0     =  15.0 * ( FLOAT( JTIME / 10000 ) +
+     &                     D60 * ( FLOAT( MOD( JTIME / 100, 100 ) )
+     &                           + D60 * FLOAT( MOD( JTIME , 100 ) ) )
+     &                   - AMM )
+C
+C...........   Read CFRAC,CLDT,CLDB.
+C
+
+C Read CLDT
+
+      IF ( .NOT. READ3( 'MET_CRO_3D', CLDTVAR , ALLAYS3,
+     &                    JDATE, JTIME, CLDT         ) ) THEN
+
+            CALL M3EXIT( PNAME, JDATE, JTIME,
+     &                  'Could not read CLDT from SFCCRO', 2 )
+
+      END IF  !  if READ3 failed
+
+C Read CLDB
+
+      IF ( .NOT. READ3( 'MET_CRO_3D', CLDBVAR , ALLAYS3,
+     &                  JDATE, JTIME, CLDB           ) ) THEN
+
+            CALL M3EXIT( PNAME, JDATE, JTIME,
+     &                  'Could not read CLDB from SFCCRO', 2 )
+
+      END IF  !  if READ3 failed
+
+C Read CFRAC
+
+      IF ( .NOT. READ3( 'MET_CRO_3D', CFRACVAR , ALLAYS3,
+     &                  JDATE, JTIME, CFRAC           ) ) THEN
+
+            CALL M3EXIT( PNAME, JDATE, JTIME,
+     &                  'Could not read CFRAC from SFCCRO', 2 )
+
+      END IF  !  if READ3 failed
+
+C --- compute cld attenuation factors for all cols,rows in domain
+
+      DO  ROW = 1,NY
+        DO  COL = 1,NX
+          H1  = PI180 * ( H0 + LON( COL, ROW ) ) 
+          CZ  = DESIN * SIN ( PI180 * LAT( COL, ROW ))
+     &          + DECOS * COS ( PI180 * LAT( COL, ROW )) * COS( H1 )
+          IF( CFRAC( COL, ROW ) .LE. 0.001) THEN
+             CLDATN( COL, ROW ) = 1.
+          ELSE
+             CZ = 1.0 / MAX( CZ, 0.2 )
+             FRAC = CFRAC( COL, ROW )
+             CBOT =  CLDB( COL, ROW )
+             CTOP =  CLDT( COL, ROW )
+             T = 1.0
+             CTHK = CTOP - CBOT
+             IF( CTHK .GT. THIN ) THEN
+               LTHIN = .FALSE.
+             ELSE
+               LTHIN = .TRUE.
+             END IF
+C
+C -- per consistency with BEIS2 cld attenuation using station data,
+C    calculate number of cloud layers with divisions at 1000, 3000,
+C    and 7000m by knowing composite column cloud base and top, which
+C    have already accounted for multiple layers and model levels...
+C
+             IF( CBOT .LT. 1000. ) THEN
+                IF( CTOP .LT. 1000. ) NCLAY = 1
+                IF( CTOP .GE. 1000. .AND. CTOP .LT. 3000. ) NCLAY = 2
+                IF( CTOP .GE. 3000. ) NCLAY = 3
+             ELSE IF( CBOT .GE. 1000. .AND. CBOT .LT. 3000. ) THEN
+                IF( CTOP .LT. 3000. ) NCLAY = 1
+                IF( CTOP .GE. 3000. .AND. CTOP .LT. 7000. ) NCLAY = 2
+                IF( CTOP .GE. 7000. ) NCLAY = 3
+             ELSE IF( CBOT .GE. 3000. .AND. CBOT .LT. 7000. ) THEN
+                IF( CTOP .GE. 3000. .AND. CTOP .LT. 7000. ) NCLAY = 1
+                IF( CTOP .GE. 7000. ) NCLAY = 2
+             ELSE IF( CBOT .GE. 7000. ) THEN
+                NCLAY = 1
+             END IF
+C
+             DO 11  I = 1,NCLAY
+               IF( CBOT .LT. 1000.0) THEN
+                 TTYPE = AA(I+1) + BB(I+1) * CZ
+               ELSE IF( CBOT .GE. 1000. .AND. CBOT .LT. 3000. ) THEN
+                 TTYPE = AA(I+2) + BB(I+2) * CZ
+               ELSE IF( CBOT .GE. 3000. .AND. CBOT .LT. 7000. ) THEN
+                 TTYPE = AA(I+3) + BB(I+3) * CZ
+               ELSE
+                 TTYPE = AA(5) + BB(5) * CZ
+               END IF
+               T = T * ( 1.0 - ( 1.0 - TTYPE ) * FRAC)
+11           CONTINUE
+
+             IF( LTHIN ) THEN
+               CLDATN(COL,ROW) = 0.5 + 0.5 * T
+             ELSE
+               CLDATN(COL,ROW) = T
+             END IF
+          END IF
+
+        ENDDO
+      ENDDO
+C
+
+      DO  R = 1, NY
+      DO  C = 1, NX
+         T = CLDATN( C,R )
+         IF ( T .LT. 0.05 ) THEN
+             CLDATN( C,R ) = 0.05
+         ELSE IF ( T .GT.  1.0 ) THEN
+             CLDATN( C,R ) = 1.0
+         END IF
+      ENDDO
+      ENDDO
+
+155   CONTINUE
+
+      RETURN
+      END
---
+++ src/biog/prebmet.f
@@ -0,0 +1,316 @@
+
+        SUBROUTINE PREBMET( MNAME, WNAME, SFILE, TZONE, TSTEP, SDATE, 
+     &                      STIME, NSTEPS, MDATE, MTIME, WDATE, WTIME )
+
+C***********************************************************************
+C  subroutine body starts at line 104 
+C
+C  DESCRIPTION:
+C       Shifts met data to desired input time zone
+C
+C  PRECONDITIONS REQUIRED:
+C
+C  SUBROUTINES AND FUNCTIONS CALLED:
+C
+C  REVISION  HISTORY:
+C     Created 11/99 by J. Vukovich
+C
+C****************************************************************************/
+C
+C Project Title: Sparse Matrix Operator Kernel Emissions (SMOKE) Modeling
+C                System
+C File: @(#)$Id$
+C
+C COPYRIGHT (C) 1999, MCNC--North Carolina Supercomputing Center
+C All Rights Reserved
+C
+C See file COPYRIGHT for conditions of use.
+C
+C Environmental Programs Group
+C MCNC--North Carolina Supercomputing Center
+C P.O. Box 12889
+C Research Triangle Park, NC  27709-2889
+C
+C env_progs@mcnc.org
+C
+C Pathname: $Source$
+C Last updated: $Date$ 
+C
+C***********************************************************************
+
+        IMPLICIT NONE
+
+C...........   INCLUDES
+
+        INCLUDE 'EMCNST3.EXT'   !  emissions constant parameters
+        INCLUDE 'PARMS3.EXT'    !  I/O API parameters
+        INCLUDE 'IODECL3.EXT'   !  I/O API function declarations
+        INCLUDE 'FDESC3.EXT'    !  I/O API file description data structures
+
+C...........   EXTERNAL FUNCTIONS and their descriptions:
+        CHARACTER*2     CRLF
+        INTEGER         SECSDIFF
+        INTEGER         TIME2SEC
+
+        EXTERNAL  CRLF, SECSDIFF, TIME2SEC
+
+C...........   SUBROUTINE ARGUMENTS
+        CHARACTER(*), INTENT    (IN) :: MNAME  ! gridded tmpr file
+        CHARACTER(*), INTENT    (IN) :: WNAME  ! min/max tmpr file
+        LOGICAL     , INTENT    (IN) :: SFILE  ! one or two met files
+        INTEGER     , INTENT    (IN) :: TZONE  ! output time zone
+        INTEGER     , INTENT    (IN) :: TSTEP  ! processing time step
+        INTEGER     , INTENT(IN OUT) :: SDATE  ! output start date
+        INTEGER     , INTENT(IN OUT) :: STIME  ! output start time
+        INTEGER     , INTENT(IN OUT) :: NSTEPS ! output no. time steps
+        INTEGER     , INTENT   (OUT) :: MDATE  ! 1st valid date - MNAME file
+        INTEGER     , INTENT   (OUT) :: MTIME  ! 1st valid time - MNAME file
+        INTEGER     , INTENT   (OUT) :: WDATE  ! 1st valid date - WNAME file
+        INTEGER     , INTENT   (OUT) :: WTIME  ! 1st valid time - WNAME file
+
+C...........   Other local variables
+        INTEGER         EDATEZ       ! Sim end date in zone IZONE
+        INTEGER         ETIMEZ       ! Sim end time in zone IZONE
+        INTEGER         HRS, DYS     ! tmp no. hours & days
+        INTEGER         ISDIFF       ! tmp no. seconds difference
+        INTEGER         ISECS        ! tmp no. seconds difference
+        INTEGER         IZONE        ! time zone of met data
+        INTEGER         ML, WL       ! tmp string lengths
+        INTEGER         NSTEPSNEW    ! tmp new no. time steps
+        INTEGER         SECS         ! tmp value of seconds
+        INTEGER         SDATENEW     ! tmp new SDATE
+        INTEGER         STIMENEW     ! tmp new STIME
+        INTEGER         SDATEZ       ! SDATE in zone IZONE
+        INTEGER         STIMEZ       ! STIME in zone IZONE
+        INTEGER         MEDATEZ      ! MNAME ending date 
+        INTEGER         METIMEZ      ! MNAME ending time
+        INTEGER         MSDATEZ      ! MNAME start date 
+        INTEGER         MSTIMEZ      ! MNAME start time
+        INTEGER         WEDATEZ      ! WNAME ending date 
+        INTEGER         WETIMEZ      ! WNAME ending time
+        INTEGER         WSDATEZ      ! WNAME start date 
+        INTEGER         WSTIMEZ      ! WNAME start time
+
+        LOGICAL      :: EFLAG   = .FALSE. ! true: error occurred
+        LOGICAL      :: MADJUST = .FALSE. ! true: date/time adjusted for MNAME
+        LOGICAL      :: WADJUST = .FALSE. ! true: date/time adjusted for WNAME
+
+        CHARACTER*300   MESG     !  message buffer
+
+        CHARACTER*16 :: PROGNAME = 'PREBMET' ! program name
+
+C***********************************************************************
+C   begin body of subroutine PRETMPR
+
+C.........  Get lengths of file names
+        ML = LEN_TRIM( MNAME )
+        WL = LEN_TRIM( WNAME )
+
+C.........  Convert start date and time to time zone of meteorology.
+C.........  Met data time zone (IZONE) assume GMT
+        IZONE = 0
+        SDATEZ = SDATE
+        STIMEZ = STIME
+        CALL NEXTIME( SDATEZ, STIMEZ, (TZONE - IZONE )*10000 )
+
+C.........  Compute end date and time of simulation (in GMT)
+        EDATEZ = SDATEZ
+        ETIMEZ = STIMEZ
+        CALL NEXTIME( EDATEZ, ETIMEZ, (NSTEPS-1)*10000 )
+
+C.........  Get header information from gridded temperature file
+        IF( .NOT. DESC3( MNAME ) ) THEN
+            MESG = 'Could not get description of file "' //
+     &             MNAME( 1:ML ) // '"'
+            CALL M3EXIT( PROGNAME, 0, 0, MESG, 2 )
+        END IF
+
+C.........  Store start gridded meterology date/time (in GMT)
+        MSDATEZ = SDATE3D
+        MSTIMEZ = STIME3D
+
+C.........  Calculate end gridded meteorology date/time (in GMT)
+        MEDATEZ = MSDATEZ
+        METIMEZ = MSTIMEZ
+        CALL NEXTIME( MEDATEZ, METIMEZ, (MXREC3D-1)*10000 )
+
+        IF ( .NOT. SFILE ) THEN
+C.........  Get header information from meteorology  file
+          IF( .NOT. DESC3( WNAME ) ) THEN
+            MESG = 'Could not get description of file "' //
+     &             WNAME( 1:WL ) // '"'
+            CALL M3EXIT( PROGNAME, 0, 0, MESG, 2 )
+          END IF
+
+C.........  Store start min/max meterology date/time (in GMT)
+          WSDATEZ = SDATE3D
+          WSTIMEZ = STIME3D
+
+C.........  Calculate end min/max meteorology date/time (in GMT)
+          WEDATEZ = WSDATEZ
+          WETIMEZ = WSTIMEZ
+          CALL NEXTIME( WEDATEZ, WETIMEZ, (MXREC3D-1)*10000 )
+
+        ENDIF
+
+C.........  Check if simulation start date/time < gridded temperature
+C           start date/time.  If so, adjust simulation start date/time to
+C           be consistent with the meteorology.
+
+        IF( ( SDATEZ .EQ. MSDATEZ .AND. STIMEZ .LT. MSTIMEZ ) .OR.
+     &        SDATEZ .LT. MSDATEZ ) THEN
+
+            MADJUST = .TRUE.
+            SDATEZ = MSDATEZ
+            STIMEZ = MSTIMEZ
+
+        END IF
+
+C.........  Check if simulation end date/time > gridded temperature ending
+C           date/time.  If so, adjust simulation ending date/time to be
+C           consistent with the meteorology.
+        IF( ( EDATEZ .EQ. MEDATEZ .AND. ETIMEZ .GT. METIMEZ ) .OR.
+     &        EDATEZ .GT. MEDATEZ ) THEN
+
+            MADJUST = .TRUE.
+            EDATEZ = MEDATEZ
+            ETIMEZ = METIMEZ
+
+        END IF
+
+        IF ( .NOT. SFILE ) THEN
+
+C.........  Check if simulation start date/time < gridded temperature
+C           start date/time.  If so, adjust simulation start date/time to
+C           be consistent with the meteorology.
+          IF( ( SDATEZ .EQ. WSDATEZ .AND. STIMEZ .LT. WSTIMEZ ) .OR.
+     &         SDATEZ .LT. WSDATEZ ) THEN
+
+            WADJUST = .TRUE.
+            SDATEZ = WSDATEZ
+            STIMEZ = WSTIMEZ
+
+          END IF
+
+C.........  Check if simulation end date/time > gridded temperature ending
+C           date/time.  If so, adjust simulation ending date/time to be
+C           consistent with the meteorology.
+          IF( ( EDATEZ .EQ. WEDATEZ .AND. ETIMEZ .GT. WETIMEZ ) .OR.
+     &        EDATEZ .GT. WEDATEZ ) THEN
+
+            WADJUST = .TRUE.
+            EDATEZ = WEDATEZ
+            ETIMEZ = WETIMEZ
+
+          END IF
+
+        ENDIF
+
+
+C.........  Compute start date/time in output time zone
+        SDATENEW = SDATEZ
+        STIMENEW = STIMEZ
+        CALL NEXTIME( SDATENEW, STIMENEW, ( IZONE - TZONE )*10000 )
+
+C.........  Compute new number of time steps
+        ISDIFF    = SECSDIFF( SDATEZ, STIMEZ, EDATEZ, ETIMEZ )
+        ISECS     = TIME2SEC( TSTEP )
+        NSTEPSNEW = ISDIFF / ISECS + 1
+
+
+C.........  Write messages stating changes because of meteorology files
+        IF( MADJUST .OR. WADJUST ) THEN
+
+            IF( MADJUST ) THEN
+                MESG = 'WARNING: Adjusting simulation start and/or '//
+     &                 'end date/time for '// CRLF()// BLANK10 //
+     &                 'file "' // MNAME( 1:ML ) // '" ...'
+                CALL M3MSG2( MESG )
+            END IF
+
+            IF ( .NOT. SFILE ) THEN
+              IF( WADJUST ) THEN
+                MESG = 'WARNING: Adjusting simulation start and/or '//
+     &                 'end date for '// CRLF()// BLANK10 //
+     &                 'file "' // WNAME( 1:WL ) // '" ...'
+                CALL M3MSG2( MESG )
+              END IF
+
+            ENDIF
+
+            WRITE( MESG,94010 ) BLANK5 //
+     &        'For time zone :', TZONE   , CRLF()// BLANK10 //
+     &        'Old start date:', SDATE   , CRLF()// BLANK10 //
+     &        'Old start time:', STIME   , CRLF()// BLANK10 //
+     &        'Old duration  :', NSTEPS  , CRLF()// BLANK10 //
+     &        'New start date:', SDATENEW, CRLF()// BLANK10 //
+     &        'New start time:', STIMENEW, CRLF()// BLANK10 //
+     &        'New duration  :', NSTEPSNEW
+
+            CALL M3MSG2( MESG )
+
+        ENDIF
+
+
+C.........  Now that date/time in met time zone is consistent with met...
+C.........  Compare simulation start date and time to that of gridded met
+C           data and give a warning if met file starts sooner.
+C.........  Reset output met start date and time based on simulation
+        IF( SDATEZ .NE. MSDATEZ .OR. STIMEZ .NE. MSTIMEZ ) THEN
+
+            ISDIFF = SECSDIFF( SDATEZ, STIMEZ, MSDATEZ, MSTIMEZ )
+            SECS   = TIME2SEC( TSTEP )
+            HRS    = ISDIFF / SECS
+            
+            WRITE( MESG,94010 ) 'WARNING: Gridded meteorology file '//
+     &             'starts', HRS, 'hours before simulation.'
+            CALL M3MSG2( MESG )
+
+            MDATE = SDATEZ
+            MTIME = STIMEZ
+
+C.........  Otherwise, set met start date and time based on met file
+        ELSE
+            MDATE = MSDATEZ
+            MTIME = MSTIMEZ
+
+        END IF
+
+        IF ( .NOT. SFILE ) THEN 
+
+          IF( SDATEZ .NE. WSDATEZ .OR. STIMEZ .NE. WSTIMEZ ) THEN
+
+            ISDIFF = SECSDIFF( SDATEZ, STIMEZ, WSDATEZ, WSTIMEZ )
+            SECS   = TIME2SEC( TSTEP )
+            HRS    = ISDIFF / SECS
+
+            WRITE( MESG,94010 ) 'WARNING: Gridded meteorology file '//
+     &             'starts', HRS, 'hours before simulation.'
+            CALL M3MSG2( MESG )
+
+            WDATE = SDATEZ
+            WTIME = STIMEZ
+
+C.........  Otherwise, set met start date and time based on met file
+          ELSE
+            WDATE = WSDATEZ
+            WTIME = WSTIMEZ
+
+          END IF
+
+        ENDIF
+
+C.........   Reset simulation date/time/duration to new values
+        SDATE  = SDATENEW
+        STIME  = STIMENEW
+        NSTEPS = NSTEPSNEW
+
+        RETURN
+
+C******************  FORMAT  STATEMENTS   ******************************
+
+C...........   Internal buffering formats............ 94xxx
+
+94010   FORMAT( 10( A, :, I8, :, 1X ) )
+
+        END SUBROUTINE PREBMET
