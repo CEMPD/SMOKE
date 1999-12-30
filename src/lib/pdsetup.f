338983be12c7b0a62e74836b922ff791249aaf27
mhouyoux <>
1999-12-30 16:43:36 +0000

    Removed SETVPTR call Updated to check time zone Updated to use pol/act variables instead of pol-only variables

--- src/lib/pdsetup.f
+++ src/lib/pdsetup.f
@@ -1,6 +1,7 @@
 
         SUBROUTINE PDSETUP( INFILE, ESDATE, ESTIME, EEDATE, EETIME, 
-     &                      NIPOL, EINAM, NVARS, NENTRY, EFLAG, PNAME )
+     &                      TZONE, NIPPA, EANAM, NPOA, NENTRY, 
+     &                      EFLAG, PNAME, PDESC )
 
 C***********************************************************************
 C  subroutine body starts at line
@@ -48,62 +49,81 @@ C...........   INCLUDE FILES:
 
 C...........   EXTERNAL FUNCTIONS:
         CHARACTER*2     CRLF
+        INTEGER         GETIFDSC
         CHARACTER*10    HHMMSS
+        INTEGER         INDEX1
         CHARACTER*14    MMDDYY
         INTEGER         SECSDIFF
 
-        EXTERNAL        CRLF, HHMMSS, MMDDYY, SECSDIFF
+        EXTERNAL        CRLF, GETIFDSC, HHMMSS, INDEX1, MMDDYY, SECSDIFF
 
 C...........   SUBROUTINE ARGUMENTS
         CHARACTER(*), INTENT (IN) :: INFILE ! name of day- or hour-spec file	
         INTEGER     , INTENT (IN) :: ESDATE ! episode starting date
         INTEGER     , INTENT (IN) :: ESTIME ! episode starting time
-        INTEGER     , INTENT (IN) :: EEDATE ! episode ending date )
-        INTEGER     , INTENT (IN) :: EETIME ! episode ending time (in)
-        INTEGER     , INTENT (IN) :: NIPOL  ! number of inventory pollutants
-        CHARACTER(*), INTENT (IN) :: EINAM( NIPOL ) ! names of inventory pols
-	INTEGER     , INTENT(OUT) :: NVARS  ! no. variables in file
+        INTEGER     , INTENT (IN) :: EEDATE ! episode ending date
+        INTEGER     , INTENT (IN) :: EETIME ! episode ending time
+        INTEGER     , INTENT (IN) :: TZONE  ! output time zone
+        INTEGER     , INTENT (IN) :: NIPPA  ! number of inventory pols/acts
+        CHARACTER(*), INTENT (IN) :: EANAM( NIPPA ) ! names of pols/acts
+	INTEGER     , INTENT(OUT) :: NPOA   ! no. pols/acts
 	INTEGER     , INTENT(OUT) :: NENTRY ! no. entries in file
 	LOGICAL     , INTENT(OUT) :: EFLAG  ! True if error occurred in routine
-        CHARACTER(*), INTENT(OUT) :: PNAME( * ) ! names of time-specific pols
- 
-C...........   Index of variable names in EINAM
-        INTEGER       INDX( NIPOL )
+        CHARACTER(*), INTENT(OUT) :: PNAME( * ) ! names of time-spec pol/acts
+        CHARACTER(*), INTENT(OUT) :: PDESC( * ) ! descriptions of pol/acts
 
 C...........   Other local variables
-        INTEGER         I, J, L, L2      !  counters and indices
+        INTEGER         I, J, LD, L2, N      !  counters and indices
 
         INTEGER         EDATE            !  ending date
         INTEGER         ETIME            !  ending time
         INTEGER         IOS              !  i/o status
         INTEGER         NSTEPS           !  number of time steps
+        INTEGER         NVARS            !  tmp no. of variables in file
         INTEGER         SDATE            !  starting date
         INTEGER         STIME            !  starting time
+        INTEGER         ZONE             !  time zone
 
         CHARACTER*4     DESCRIBE 
         CHARACTER*10    CTIMESTR, ETIMESTR   ! inventory and comparison time
         CHARACTER*14    CDATESTR, EDATESTR   ! inventory and comparison date
         CHARACTER*300   MESG 
 
+        CHARACTER(LEN=IOVLEN3)   VBUF        ! tmp variable name
+
         CHARACTER*16 :: PROGNAME = 'PDSETUP' ! program name
 
 C***********************************************************************
 C   begin body of subroutine PDSETUP
 
-C.........  Initialize index
-        INDX = 0    ! array
-
-C.........  NOTE: FDESC3 variables passed through I/O API common in include file
+C.........  NOTE- FDESC3 variables passed through I/O API common in include file
 C.........  Make settings that depend on day-specific or hour-specific data
         IF( TSTEP3D .EQ. 240000 ) THEN  ! day specific
             NSTEPS = 24 * MXREC3D  
             DESCRIBE = 'Day'
+            LD = LEN_TRIM( DESCRIBE )
 
         ELSE ! hour specific
             NSTEPS= MXREC3D
             DESCRIBE = 'Hour'
+            LD = LEN_TRIM( DESCRIBE )
 
-        ENDIF
+        END IF
+
+C.........  Ensure that the time zone of the file is consistent with the
+C           time zone of the output data
+        ZONE = GETIFDSC( FDESC3D, '/TZONE/', .TRUE. )
+
+        IF( ZONE .NE. TZONE ) THEN
+
+            EFLAG = .TRUE.
+            WRITE( MESG,94010 ) 'ERROR: Time zone in ' //
+     &             DESCRIBE( 1:LD ) // '-specific file is', ZONE,
+     &             CRLF() // BLANK10 // 'and is inconsistent ' //
+     &             'with output time zone ', TZONE
+            CALL M3MSG2( MESG )
+
+        END IF
 
 C.........  Store header info for memory allocation and flexible usage
 C               of pollutant-specific data
@@ -118,64 +138,64 @@ C.........  Set ending date/time of file
         CALL NEXTIME( EDATE, ETIME, NSTEPS * 10000 )
 
 C.........  Compare day-specific dates/times to epsiode dates/times
-        I = SECSDIFF( ESDATE, ESTIME, SDATE, SDATE )
-        J = SECSDIFF( EEDATE, EETIME, EDATE, EDATE )
+        I = SECSDIFF( ESDATE, ESTIME, SDATE, STIME )
+        J = SECSDIFF( EEDATE, EETIME, EDATE, ETIME )
 
-        L = LEN_TRIM( DESCRIBE )
-        IF( I .LT. 0 ) THEN
+        IF( I .GT. 0 ) THEN
             EFLAG = .TRUE.
             EDATESTR = MMDDYY( ESDATE )
             ETIMESTR = HHMMSS( ESTIME )
             CDATESTR = MMDDYY( SDATE )
             CTIMESTR = HHMMSS( STIME )
-            MESG = 'ERROR: ' // DESCRIBE( 1:L ) // '-specific ' //
-     &             'starting date/time of '// CDATESTR// '/' //
+            MESG = 'ERROR: ' // DESCRIBE( 1:LD ) // '-specific ' //
+     &             'starting date/time of '// CDATESTR// '@ ' //
      &             CTIMESTR // CRLF() // BLANK10 // 
      &             'is later than episode starting date/time of ' //
-     &             EDATESTR // '/' // ETIMESTR // '.'
+     &             EDATESTR // '@ ' // ETIMESTR 
 
             CALL M3MSG2( MESG )
-        ENDIF
+        END IF
 
-        IF( J .GT. 0 ) THEN
+        IF( J .LT. 0 ) THEN
             EFLAG = .TRUE.
             EDATESTR = MMDDYY( EEDATE )
             ETIMESTR = HHMMSS( EETIME )
             CDATESTR = MMDDYY( EDATE )
             CTIMESTR = HHMMSS( ETIME )
-            MESG = 'ERROR: ' // DESCRIBE( 1:L ) // '-specific ' //
-     &             'ending date/time of '// CDATESTR// '/' //
+            MESG = 'ERROR: ' // DESCRIBE( 1:LD ) // '-specific ' //
+     &             'ending date/time of '// CDATESTR// '@ ' //
      &             CTIMESTR // CRLF() // BLANK10 // 
      &             'is earlier than episode ending date/time of ' //
-     &             EDATESTR // '/' // ETIMESTR // '.'
+     &             EDATESTR // '/' // ETIMESTR 
 
             CALL M3MSG2( MESG )
-        ENDIF
-
-C.........  Match hour-specific pol names to inventory pol names
-        CALL SETVPTR( NIPOL, NVARS, EINAM, VNAME3D, INDX, IOS )
+        END IF
 
 C.............  Store list of valid pollutant names, or...
-C.............  Report which day-specific variables will be ignored
-        DO I = 1, NVARS
+C.............  Report which period-specific variables will be ignored
+C.............  Skip first variable, which is the source index
+        N = 0
+        DO I = 2, NVARS
 
-            J = INDX( I ) 
-            IF( J .EQ. 0 ) THEN
-                L2 = LEN_TRIM( VNAME3D( I ) )
-                MESG = 'WARNING: ' // DESCRIBE( 1:L ) // 
-     &                 '-specific pollutant "' //
-     &                 VNAME3D( I )( 1:L2 ) // 
+            VBUF = VNAME3D( I )
+            J = INDEX1( VBUF, NIPPA, EANAM )
+            IF( J .LE. 0 ) THEN
+                L2 = LEN_TRIM( VBUF )
+                MESG = 'WARNING: ' // DESCRIBE( 1:LD ) // 
+     &                 '-specific pollutant "' // VBUF( 1:L2 ) // 
      &                 '" is not in inventory file, so ' //
      &                 'it will be ignored.'
                 CALL M3MSG2( MESG )
 
             ELSE
+                N = N + 1
+                PNAME( N ) = VBUF
+                PDESC( N ) = VDESC3D( I )
 
-                PNAME( I ) = EINAM( J )
+            END IF
+        END DO
 
-            ENDIF
-
-        ENDDO                    
+        NPOA = N
 
         RETURN
 
@@ -185,5 +205,4 @@ C...........   Internal buffering formats............ 94xxx
 
 94010   FORMAT( 10( A, :, I8, :, 1X ) )
 
-        END
-
+        END SUBROUTINE PDSETUP
