717328c09693a384412ece77cf21e86f8d143c99
mhouyoux <>
2000-06-13 15:35:35 +0000

    Added formula interpretation and writing

--- src/smkinven/wrinvemis.f
+++ src/smkinven/wrinvemis.f
@@ -19,13 +19,13 @@ C
 C  REVISION  HISTORY:
 C      Created 12/99 by M. Houyoux
 C
-C****************************************************************************/
+C*************************************************************************
 C
 C Project Title: Sparse Matrix Operator Kernel Emissions (SMOKE) Modeling
 C                System
 C File: @(#)$Id$
 C
-C COPYRIGHT (C) 1999, MCNC--North Carolina Supercomputing Center
+C COPYRIGHT (C) 2000, MCNC--North Carolina Supercomputing Center
 C All Rights Reserved
 C
 C See file COPYRIGHT for conditions of use.
@@ -61,28 +61,46 @@ C...........   INCLUDES
 C.........  EXTERNAL FUNCTIONS
         CHARACTER*2   CRLF
         LOGICAL       ENVYN
+        INTEGER       INDEX1
 
-        EXTERNAL      CRLF, ENVYN
+        EXTERNAL      CRLF, ENVYN, INDEX1
 
 C.........  SUBROUTINE ARGUMENTS
         CHARACTER(*), INTENT (IN) :: FNAME   ! logical file name of output file
 
+C...........   LOCAL PARAMETERS
+        CHARACTER*16, PARAMETER :: FORMEVNM = 'SMKINVEN_FORMULA'
+
 C.........  Inventory temporay arrays
         INTEGER, ALLOCATABLE:: IPPTR ( : ) ! position in POLVAL sparse array
         INTEGER, ALLOCATABLE:: IPMAX ( : ) ! max IPPTR by source
-        REAL   , ALLOCATABLE:: SRCPOL( :,: )!  pollutant-spec values by source
+        REAL   , ALLOCATABLE:: SRCPOL( :,: )  ! data-spec values by source
+        REAL   , ALLOCATABLE:: COMPUTED( :,: )! computed data-spec values by src
 
 C...........   Other local variables
-        INTEGER         S, L2            ! counters and indices
-        INTEGER         IOS              ! i/o status
+        INTEGER         S, L, L2, VA, VB     ! counters and indices
+
+        INTEGER         IOS       ! i/o status
+        INTEGER         LEQU      ! position of '=' in formula
+        INTEGER         LDIV      ! position of '-' or '+' in formula
+        INTEGER         LMNS      ! position of '-' in formula
+        INTEGER         LPLS      ! position of '+' in formula
 
         INTEGER         RIMISS3          ! real value of integer missing
 
-        LOGICAL      :: EFLAG = .FALSE.  ! TRUE iff ERROR
-        LOGICAL         SFLAG            ! true: error on missing species
+        LOGICAL      :: CHKPLUS  = .FALSE. ! true: formula uses a + sign
+        LOGICAL      :: CHKMINUS = .FALSE. ! true: formula uses a - sign
+        LOGICAL      :: EFLAG    = .FALSE. ! true: error found
+        LOGICAL      :: FFLAG    = .FALSE. ! true: formula in use
+        LOGICAL      :: SFLAG    = .FALSE. ! true: error on missing species
 
-        CHARACTER*100   BUFFER           !  message buffer
-        CHARACTER*300   MESG             !  message buffer
+        CHARACTER*60    VAR_FORMULA      ! formula
+        CHARACTER*100   BUFFER           ! message buffer
+        CHARACTER*300   MESG             ! message buffer
+
+        CHARACTER(LEN=IOVLEN3 ) VIN_A
+        CHARACTER(LEN=IOVLEN3 ) VIN_B
+        CHARACTER(LEN=IOVLEN3 ) VNAME
 
         CHARACTER*16 :: PROGNAME = 'WRINVEMIS' !  program name
 
@@ -90,6 +108,8 @@ C***********************************************************************
 C   begin body of program WRINVEMIS
 
 C.........  Get environment variables that control the program
+        CALL ENVSTR( FORMEVNM, MESG, ' ', VAR_FORMULA, IOS )
+
         SFLAG = ENVYN( 'RAW_SRC_CHECK', 
      &                 'Flag to check for missing species-records',
      &                 .FALSE., IOS )
@@ -121,11 +141,76 @@ C           and activities).
             IPMAX( S ) = IPPTR( S )   + NPCNT( S ) - 1
         ENDDO
 
+C.........  If there is a computed output variable, get set up for that
+        IF( VAR_FORMULA .NE. ' ' ) THEN
+
+            FFLAG = .TRUE.
+
+C.............  Make sure formula makes sense
+            LEQU = INDEX( VAR_FORMULA, '=' )
+            LPLS = INDEX( VAR_FORMULA, '+' )
+            LMNS = INDEX( VAR_FORMULA, '-' )
+
+            CHKPLUS  = ( LPLS .GT. 0 )
+            CHKMINUS = ( LMNS .GT. 0 )
+
+            LDIV = LPLS
+            IF( CHKMINUS ) LDIV = LMNS
+
+            IF( LEQU .LE. 0 .OR. 
+     &        ( .NOT. CHKPLUS .AND. .NOT. CHKMINUS ) ) THEN
+
+                L = LEN_TRIM( FORMEVNM )
+                MESG = 'Could not interpret formula for extra ' //
+     &                 'pollutant from environment variable ' //
+     &                 CRLF() // BLANK10 // '"' // FORMEVNM( 1:L ) //
+     &                 '": ' // VAR_FORMULA
+                EFLAG = .TRUE.
+            END IF
+
+C.............  Extract formula variable names
+            L     = LEN_TRIM( VAR_FORMULA )
+            VNAME = ADJUSTL ( VAR_FORMULA(      1:LEQU-1 ) )
+            VIN_A = ADJUSTL ( VAR_FORMULA( LEQU+1:LDIV-1 ) )
+            VIN_B = ADJUSTL ( VAR_FORMULA( LDIV+1:L      ) )
+
+C.............  Find formula inputs in existing variable list
+            VA = INDEX1( VIN_A, NIPPA, EANAM )
+            VB = INDEX1( VIN_B, NIPPA, EANAM )
+
+            IF( VA .LE. 0 ) THEN
+                EFLAG = .TRUE.
+                L = LEN_TRIM( VIN_A )
+                MESG = 'Variable "'// VIN_A( 1:L ) // 
+     &                 '" from formula was not found in inventory.'
+                CALL M3MSG2( MESG )
+            END IF
+
+            IF( VB .LE. 0 ) THEN
+                EFLAG = .TRUE.
+                L = LEN_TRIM( VIN_B )
+                MESG = 'Variable "'// VIN_B( 1:L ) // 
+     &                 '" from formula was not found in inventory.'
+                CALL M3MSG2( MESG )
+            END IF
+
+            IF( EFLAG ) THEN
+                MESG = 'Problem processing formula.'
+                CALL M3EXIT( PROGNAME, 0, 0, MESG, 2 )
+            END IF
+
+C.............  Allocate memory for computed variable
+            ALLOCATE( COMPUTED( NSRC, NPTPPOL3 ), STAT=IOS )
+            CALL CHECKMEM( IOS, 'COMPUTED', PROGNAME )
+            COMPUTED = 0.   ! array
+
+        END IF
+
 C.........  Loop through pollutants, store, and write to inventory file
-        CALL LOOP_FOR_OUTPUT( NIPOL, EIIDX, EINAM )
+        IF( NIPOL .GT. 0 ) CALL LOOP_FOR_OUTPUT( NIPOL, EIIDX, EINAM )
 
 C.........  Loop through activity data, store, and write to inventory file
-        CALL LOOP_FOR_OUTPUT( NIACT, AVIDX, ACTVTY )
+        IF( NIACT .GT. 0 ) CALL LOOP_FOR_OUTPUT( NIACT, AVIDX, ACTVTY )
 
         IF( EFLAG ) THEN
             MESG = 'Missing data for some sources is not allowed ' //
@@ -135,11 +220,36 @@ C.........  Loop through activity data, store, and write to inventory file
             CALL M3EXIT( PROGNAME, 0, 0, MESG, 2 )
         END IF
 
+C.........  If needed, check for negative values and output computed variable
+        IF( FFLAG ) THEN
+
+C.............  Check for negative values
+            DO S = 1, NSRC
+
+                IF( COMPUTED( S,1 ) .LT. 0 ) THEN
+                    EFLAG = .TRUE.
+                    L = LEN_TRIM( VNAME )
+                    CALL FMTCSRC( CSOURC( S ), 7, BUFFER, L2 )
+                    MESG = 'ERROR: Negative value of computed data ' //
+     &                     'for "' // VNAME( 1:L )// '" for source:'// 
+     &                     CRLF() // BLANK5 // BUFFER( 1:L2 )
+                    CALL M3MESG( MESG )
+                END IF
+
+                IF( EFLAG ) THEN
+                    CALL M3EXIT( PROGNAME, 0, 0, ' ', 2 )
+                END IF
+
+            END DO
+
+C.............  Output data
+            CALL WRINVPOL( FNAME, CATEGORY, NSRC, 1, NPPOL, 
+     &                     VNAME, COMPUTED )
+        END IF
+
 C.........  Deallocate local arrays
         DEALLOCATE( IPPTR, IPMAX, SRCPOL )
-
-C.........  Deallocate global emissions arrays
-        CALL SRCMEM( CATEGORY, 'SORTED', .FALSE., .TRUE., 1, 1, 1 )
+        IF( ALLOCATED( COMPUTED ) ) DEALLOCATE( COMPUTED )
 
         RETURN
 
@@ -174,6 +284,7 @@ C.................  Initialize SRCPOL pollutant/activity-specific data array
 C                   as missing
                 SRCPOL        = BADVAL3 ! array
                 SRCPOL( :,1 ) = 0.      ! array
+                IF( NOZ .GT. 0 ) SRCPOL( :,NOZ ) = 0.      ! array
                 IF( NC1 .GT. 0 ) SRCPOL( :,NC1 ) = RIMISS3 ! array
                 IF( NC2 .GT. 0 ) SRCPOL( :,NC2 ) = RIMISS3 ! array
                 IF( NCE .GT. 0 ) SRCPOL( :,NCE ) = 0.      ! array
@@ -214,13 +325,32 @@ C                           then write message and set error flag
 
                 END DO  ! end of loop through sources
 
+C.................  Write data to inventory file
                 CALL WRINVPOL( FNAME, CATEGORY, NSRC, 1, NPPOL, 
      &                         NAMES( I ), SRCPOL )
 
+C.................  If current data variable is the first variable in the
+C                   formula, then store data in formula arrays
+                IF( NAMES( I ) .EQ. VIN_A ) THEN
+                    COMPUTED( :,1 ) = COMPUTED( :,1 ) + SRCPOL( :,1 )
+                    COMPUTED( :,1:NPPOL ) = SRCPOL( :,1:NPPOL )
+                END IF
+
+C.................  If current data variable is the second variable in the
+C                   formula, then use data in formula to compute output value
+                IF( NAMES( I ) .EQ. VIN_B ) THEN
+
+                    IF( CHKPLUS ) THEN
+                        COMPUTED( :,1 )= COMPUTED( :,1 ) + SRCPOL( :,1 )
+
+                    ELSE IF( CHKMINUS ) THEN
+                        COMPUTED( :,1 )= COMPUTED( :,1 ) - SRCPOL( :,1 )
+                    END IF
+
+                END IF
+
             END DO  ! end loop through actual output data
 
-C------------------- SUBPROGRAM FORMAT STATEMENTS ----------------------
-
             END SUBROUTINE LOOP_FOR_OUTPUT
- 
+  
         END SUBROUTINE WRINVEMIS
