62c2e8bb023b17f05dd86ceb44a60d3269c6ad95
mhouyoux <>
2000-06-13 15:31:25 +0000

    Added JSTACK Added EPS

--- src/smkinven/initinfo.f
+++ src/smkinven/initinfo.f
@@ -21,7 +21,7 @@ C Project Title: Sparse Matrix Operator Kernel Emissions (SMOKE) Modeling
 C                System
 C File: @(#)$Id$
 C
-C COPYRIGHT (C) 1999, MCNC--North Carolina Supercomputing Center
+C COPYRIGHT (C) 2000, MCNC--North Carolina Supercomputing Center
 C All Rights Reserved
 C
 C See file COPYRIGHT for conditions of use.
@@ -116,13 +116,18 @@ C.........  For point sources ...
             CASE( IDAFMT )
                 NCHARS = 6
                 JSCC   = 6
+                JSTACK = 4
 
             CASE( EPSFMT )
-C ??
+                NCHARS = 6
+                JSCC   = 6
+                JSTACK = 3
+
             CASE( EMSFMT )
                 NCHARS   = 5
+                JSTACK   = 3
                 NEMSFILE = 5   ! Number of required EMS-95 file types
-            END SELECT
+           END SELECT
           
         END SELECT
 
