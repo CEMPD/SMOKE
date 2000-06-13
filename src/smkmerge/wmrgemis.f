09e6171805da4e0a1537340ac5fb422d51469294
mhouyoux <>
2000-06-13 15:48:06 +0000

    *** empty log message ***

--- src/smkmerge/wmrgemis.f
+++ src/smkmerge/wmrgemis.f
@@ -22,7 +22,7 @@ C Project Title: Sparse Matrix Operator Kernel Emissions (SMOKE) Modeling
 C                System
 C File: @(#)$Id$
 C
-C COPYRIGHT (C) 1999, MCNC--North Carolina Supercomputing Center
+C COPYRIGHT (C) 2000, MCNC--North Carolina Supercomputing Center
 C All Rights Reserved
 C
 C See file COPYRIGHT for conditions of use.
@@ -42,6 +42,9 @@ C****************************************************************************
 C.........  MODULES for public variables
 C.........  This module contains the major data structure and control flags
         USE MODMERGE
+
+C.........  This module contains arrays for plume-in-grid and major sources
+        USE MODELEV
 
         IMPLICIT NONE
 
