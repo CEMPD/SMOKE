62d2c9a203eede89e71d3ad923319ca38ff93ecd
mhouyoux <>
2000-06-13 14:43:19 +0000

    Added JSTACK to info from header

--- src/lib/getsinfo.f
+++ src/lib/getsinfo.f
@@ -22,7 +22,7 @@ C Project Title: Sparse Matrix Operator Kernel Emissions (SMOKE) Modeling
 C                System
 C File: @(#)$Id$
 C
-C COPYRIGHT (C) 1999, MCNC--North Carolina Supercomputing Center
+C COPYRIGHT (C) 2000, MCNC--North Carolina Supercomputing Center
 C All Rights Reserved
 C
 C See file COPYRIGHT for conditions of use.
@@ -91,6 +91,7 @@ C.........  Set the number of source characteristics and allocate memory for
 C           the field positions
         NCHARS   = GETIFDSC( FDESC3D, '/NUMBER CHARS/', .TRUE. )
         JSCC     = GETIFDSC( FDESC3D, '/SCC POSITION/', .TRUE. )
+        JSTACK   = GETIFDSC( FDESC3D, '/STACK POSITION/', .FALSE. )
 
         ALLOCATE( SC_BEGP( NCHARS ), STAT=IOS )
         CALL CHECKMEM( IOS, 'SC_BEGP', PROGNAME )
