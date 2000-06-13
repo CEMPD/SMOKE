9b6a8788db45ef7f01631e7b4c53dab8b273e5eb
mhouyoux <>
2000-06-13 15:04:49 +0000

    Added many additional types of matches to be consistent    with the documentaiton

--- src/lib/asgnpsi.f
+++ src/lib/asgnpsi.f
@@ -15,13 +15,13 @@ C
 C  REVISION  HISTORY:
 C     Created 6/99 by M. Houyoux
 C
-C****************************************************************************/
+C***************************************************************************
 C
 C Project Title: Sparse Matrix Operator Kernel Emissions (SMOKE) Modeling
 C                System
 C File: @(#)$Id$
 C
-C COPYRIGHT (C) 1999, MCNC--North Carolina Supercomputing Center
+C COPYRIGHT (C) 2000, MCNC--North Carolina Supercomputing Center
 C All Rights Reserved
 C
 C See file COPYRIGHT for conditions of use.
@@ -71,31 +71,45 @@ C.........  Other local variables
         INTEGER          I, J, L2, S, V    !  counters and indices
 
         INTEGER          F0, F1, F2, F3, F4, F5, F6  ! tmp find indices
+        INTEGER       :: F0B = 0 ! extra find index for mobile
+        INTEGER       :: F2B = 0 ! extra find index for mobile
+        INTEGER       :: F4B = 0 ! extra find index for mobile
         INTEGER          EREF           !  tmp diurnal profile code
 
         LOGICAL       :: EFLAG    = .FALSE.
         LOGICAL, SAVE :: FIRSTIME = .TRUE.
         LOGICAL, SAVE :: REPDEFLT = .TRUE.
 
-        CHARACTER*10           RWTFMT   ! roadway type format
-        CHARACTER*300          BUFFER   ! source fields buffer
-        CHARACTER*300          MESG     ! message buffer
-        CHARACTER(LEN=STALEN3) CSTA     ! tmp Country/state code
-        CHARACTER(LEN=STSLEN3) CSTASCC  ! tmp Country/state code // SCC
-        CHARACTER(LEN=STSLEN3) CSTASL   ! tmp Country/state code // left SCC
-        CHARACTER(LEN=SCCLEN3) TSCCL    ! tmp left digits of TSCC
-        CHARACTER(LEN=SRCLEN3) CSRC     ! tmp source chars string
-        CHARACTER(LEN=SS0LEN3) CSSC0    ! tmp FIPS // Plant // SCC
-        CHARACTER(LEN=SS1LEN3) CSSC1    ! tmp source chars through char1 // SCC
-        CHARACTER(LEN=SS2LEN3) CSSC2    ! tmp source chars through char2 // SCC
-        CHARACTER(LEN=SS3LEN3) CSSC3    ! tmp source chars through char3 // SCC
-        CHARACTER(LEN=SS4LEN3) CSSC4    ! tmp source chars through char4 // SCC
-        CHARACTER(LEN=SS5LEN3) CSSC5    ! tmp source chars through char5 // SCC
-        CHARACTER(LEN=FIPLEN3) CFIP     ! tmp (character) FIPS code
-        CHARACTER(LEN=FPLLEN3) CFIPPLS  ! tmp FIPS code // plant id or rdtype
-        CHARACTER(LEN=FPSLEN3) CFIPSCC  ! tmp FIPS code // SCC
-        CHARACTER(LEN=FPSLEN3) CFIPSL   ! tmp FIPS code // left SCC
-        CHARACTER(LEN=SCCLEN3) TSCC     ! tmp 10-digit SCC
+        CHARACTER*10             RWTFMT   ! roadway type format
+        CHARACTER*300            BUFFER   ! source fields buffer
+        CHARACTER*300            MESG     ! message buffer
+        CHARACTER(LEN=FIPLEN3):: CFIP     ! tmp FIPS code
+        CHARACTER(LEN=STALEN3):: CSTA     ! tmp st/county code
+        CHARACTER(LEN=LNKLEN3):: CLNK     ! temporary link code
+        CHARACTER(LEN=RWTLEN3):: CRWT     ! buffer for roadway type
+        CHARACTER(LEN=SRCLEN3):: CSRC =' '! tmp source chars string
+        CHARACTER(LEN=VIDLEN3):: CVID =' '! buffer for vehicle type number
+        CHARACTER(LEN=SS5LEN3):: CHK16=' '! tmp source chars through char5// SCC
+        CHARACTER(LEN=SS4LEN3):: CHK15=' '! tmp source chars through char4// SCC
+        CHARACTER(LEN=SS3LEN3):: CHK14=' '! tmp source chars through char3// SCC
+        CHARACTER(LEN=SS2LEN3):: CHK13=' '! tmp source chars through char2// SCC
+        CHARACTER(LEN=SS1LEN3):: CHK12=' '! tmp source chars through char1// SCC
+        CHARACTER(LEN=SS0LEN3):: CHK11=' '! tmp FIPS // Plant // SCC
+        CHARACTER(LEN=FPLLEN3):: CHK10=' '! tmp FIPS code // plant id
+        CHARACTER(LEN=FPSLEN3):: CHK09=' '! tmp FIPS code // SCC
+        CHARACTER(LEN=FPSLEN3):: CHK08=' '! tmp FIPS code // left SCC
+        CHARACTER(LEN=FPSLEN3):: CHK08B=' '! tmp FIPS code // veh ID SCC
+        CHARACTER(LEN=STSLEN3):: CHK06=' '! tmp Country/state code // SCC
+        CHARACTER(LEN=STSLEN3):: CHK05=' '! tmp Country/state code // left SCC
+        CHARACTER(LEN=STSLEN3):: CHK05B=' '! tmp Country/state code// veh ID SCC
+        CHARACTER(LEN=SCCLEN3):: CHK02B=' '! tmp veh ID SCC
+        CHARACTER(LEN=SCCLEN3)   CHKRWT   ! tmp roadway type only SCC
+        CHARACTER(LEN=SCCLEN3)   CHKVID   ! tmp vehicle-type only SCC
+        CHARACTER(LEN=RWTLEN3):: RWTZERO=' '! zero roadway type
+        CHARACTER(LEN=SCCLEN3):: TSCC =' '! tmp 10-digit SCC
+        CHARACTER(LEN=SCCLEN3):: TSCCL=' '! tmp left digits of TSCC
+        CHARACTER(LEN=VIDLEN3):: VIDHOLD  ! placeholder vehicle type
+        CHARACTER(LEN=VIDLEN3):: VIDZERO  ! zero vehicle type
 
         CHARACTER*16 :: PROGNAME = 'ASGNPSI' ! program name
 
@@ -116,23 +130,17 @@ C.............  Retrieve environment variables
 C.........  Set up roadway type format
         WRITE( RWTFMT, '("(I",I2.2,")")' ) RWTLEN3
 
+C.........  Set up roadway type and vehicle types with all zeros
+        RWTZERO = REPEAT( '0', RWTLEN3 )
+        VIDZERO = REPEAT( '0', VIDLEN3 )
+        VIDHOLD = REPEAT( '-', VIDLEN3 )
+
 C.........  Initialize search fields to blank
         CSRC    = ' '
         TSCC    = ' '
-        CSSC5   = ' '
-        CSSC4   = ' '
-        CSSC3   = ' '
-        CSSC2   = ' '
-        CSSC1   = ' '
-        CSSC0   = ' '
-        CFIPPLS = ' '
         CFIP    = ' '
         CSTA    = ' '
         TSCCL   = ' '
-        CFIPSCC = ' '
-        CFIPSL  = ' '
-        CSTASCC = ' '
-        CSTASL  = ' '
 
 C.........  Loop through activities
         DO J = 1, NACT
@@ -142,57 +150,59 @@ C.............  Find index in complete list of activities
 
             DO S = 1, NSRC
 
+                CSRC  = CSOURC( S )
+                TSCC  = CSCC( S )
+                TSCCL = TSCC( 1:LSCCEND )
+                CFIP  = CSRC( 1:FIPLEN3 )
+                CSTA  = CFIP( 1:STALEN3 )
+                CHK09 = CFIP // TSCC
+                CHK08 = CFIP // TSCCL
+                CHK06 = CSTA // TSCC
+                CHK05 = CSTA // TSCCL
+
 C.................  Create selection 
                 SELECT CASE ( CATEGORY )
 
                 CASE ( 'AREA' ) 
-                    CFIP    = CSRC( 1:FIPLEN3 )
-                    CSTA    = CFIP( 1:STALEN3 )
-                    TSCC    = CSCC( S )
-                    TSCCL   = TSCC( 1:LSCCEND )
-                    CFIPSCC = CFIP // TSCC
-                    CFIPSL  = CFIP // TSCCL
-                    CSTASCC = CSTA // TSCC
-                    CSTASL  = CSTA // TSCCL
 
 C.................  For PSIs, SCC is not used for matching, but roadway type
 C                   and link IDs are used, so adjust fields accordingly
                 CASE ( 'MOBILE' )
 
-                    CSRC    = CSOURC( S )
-                    WRITE( TSCC, RWTFMT ) IRCLAS( S )
-                    CALL PADZERO( TSCC )
+                    CRWT    = CSRC( MBBEGL3( 2 ):MBENDL3( 2 ) )
+                    CLNK    = CSRC( MBBEGL3( 3 ):MBENDL3( 3 ) )
+                    CVID    = CSRC( MBBEGL3( 4 ):MBENDL3( 4 ) )
+                    CALL PADZERO( CVID )
 
-                    CFIP    = CSRC( 1:FIPLEN3 )
-                    CSTA    = CSRC( 1:STALEN3 )
-                    TSCCL   = TSCC( 1:LSCCEND )
-                    CFIPSCC = CFIP // TSCC
-                    CFIPSL  = CFIP // TSCCL 
-                    CSTASCC = CSTA // TSCC
-                    CSTASL  = CSTA // TSCCL
-                    CFIPPLS = CSRC( 1:MBENDL3( 2 ) )
-                    CSSC0   = CSRC( 1:MBENDL3( 2 ) ) // TSCC  ! w/ road class
-                    CSSC1   = CSRC( 1:MBENDL3( 3 ) ) // TSCC  ! w/ link
-                    CSSC2   = CSRC( 1:MBENDL3( 4 ) ) // TSCC  ! w/ vehicle type
+                    TSCC = CRWT // CVID
+                    CALL PADZERO( TSCC )
+                    TSCCL= TSCC( 1:LSCCEND )
+
+                    CHKVID = RWTZERO // CVID
+                    CALL PADZERO( CHKVID )
+
+                    CHKRWT = CRWT // VIDHOLD
+                    CALL PADZERO( CHKRWT )
+
+                    CHK09  = CFIP // TSCC
+                    CHK08  = CFIP // TSCCL 
+                    CHK08B = CFIP // CHKVID                  ! County// VTP
+                    CHK06  = CSTA // TSCC
+                    CHK05  = CSTA // TSCCL
+                    CHK05B = CSTA // CHKVID                   ! State // vtype
+                    CHK02B = CHKVID                           ! Vehicle type
+                    CHK12  = CSRC( 1:MBENDL3( 3 ) ) // CHKRWT ! County//rcl//lnk
+                    CHK13  = CSRC( 1:MBENDL3( 4 ) ) // TSCC   ! w/ vehicle type
 
                 CASE ( 'POINT' )
 
-                    CSRC    = CSOURC( S )
-                    TSCC    = CSCC( S )
-                    CSSC5   = CSRC( 1:PTENDL3( 7 ) ) // TSCC
-                    CSSC4   = CSRC( 1:PTENDL3( 6 ) ) // TSCC
-                    CSSC3   = CSRC( 1:PTENDL3( 5 ) ) // TSCC
-                    CSSC2   = CSRC( 1:PTENDL3( 4 ) ) // TSCC
-                    CSSC1   = CSRC( 1:PTENDL3( 3 ) ) // TSCC
-                    CSSC0   = CSRC( 1:PTENDL3( 2 ) ) // TSCC
-                    CFIPPLS = CSRC( 1:PTENDL3( 2 ) )
-                    CFIP    = CSRC( 1:FIPLEN3 )
-                    CSTA    = CSRC( 1:STALEN3 )
-                    TSCCL   = TSCC( 1:LSCCEND )
-                    CFIPSCC = CFIP // TSCC
-                    CFIPSL  = CFIP // TSCCL 
-                    CSTASCC = CSTA // TSCC
-                    CSTASL  = CSTA // TSCCL
+                    CHK16   = CSRC( 1:PTENDL3( 7 ) ) // TSCC
+                    CHK15   = CSRC( 1:PTENDL3( 6 ) ) // TSCC
+                    CHK14   = CSRC( 1:PTENDL3( 5 ) ) // TSCC
+                    CHK13   = CSRC( 1:PTENDL3( 4 ) ) // TSCC
+                    CHK12   = CSRC( 1:PTENDL3( 3 ) ) // TSCC
+                    CHK11   = CSRC( 1:PTENDL3( 2 ) ) // TSCC 
+                    CHK10   = CSRC( 1:PTENDL3( 2 ) )           ! County // plant
                     
                 CASE DEFAULT
 
@@ -214,13 +224,13 @@ C                           activity-specific CHAR1 non-blank// SCC match; then
 C                           activity-specific PLANT non-blank// SCC match; then
 C                           activity-specific PLANT non-blank       match
 
-                F6 = FINDC( CSSC5  , TXCNT( 16 ), CHRT16 ) 
-                F5 = FINDC( CSSC4  , TXCNT( 15 ), CHRT15 ) 
-                F4 = FINDC( CSSC3  , TXCNT( 14 ), CHRT14 ) 
-                F3 = FINDC( CSSC2  , TXCNT( 13 ), CHRT13 ) 
-                F2 = FINDC( CSSC1  , TXCNT( 12 ), CHRT12 ) 
-                F1 = FINDC( CSSC0  , TXCNT( 11 ), CHRT11 ) 
-                F0 = FINDC( CFIPPLS, TXCNT( 10 ), CHRT10 ) 
+                F6 = FINDC( CHK16, TXCNT( 16 ), CHRT16 ) 
+                F5 = FINDC( CHK15, TXCNT( 15 ), CHRT15 ) 
+                F4 = FINDC( CHK14, TXCNT( 14 ), CHRT14 ) 
+                F3 = FINDC( CHK13, TXCNT( 13 ), CHRT13 ) 
+                F2 = FINDC( CHK12, TXCNT( 12 ), CHRT12 ) 
+                F1 = FINDC( CHK11, TXCNT( 11 ), CHRT11 ) 
+                F0 = FINDC( CHK10, TXCNT( 10 ), CHRT10 )
 
                 IF( F6 .GT. 0 .AND. IEFS16(F6,V) .GE. ADDPS ) THEN
                     EREF = IEFS16( F6,V ) - ADDPS
@@ -266,12 +276,24 @@ C                           activity-specific Cy/st code & left SCC match; then
 C                           activity-specific SCC match; then
 C                           activity-specific left SCC match
 
-                F5 = FINDC( CFIPSCC, TXCNT( 9 ), CHRT09 ) 
-                F4 = FINDC( CFIPSL , TXCNT( 8 ), CHRT08 ) 
-                F3 = FINDC( CSTASCC, TXCNT( 6 ), CHRT06 ) 
-                F2 = FINDC( CSTASL , TXCNT( 5 ), CHRT05 ) 
-                F1 = FINDC( TSCC   , TXCNT( 3 ), CHRT03 ) 
-                F0 = FINDC( TSCCL  , TXCNT( 2 ), CHRT02 ) 
+                F5 = FINDC( CHK09, TXCNT( 9 ), CHRT09 ) 
+                F4 = FINDC( CHK08, TXCNT( 8 ), CHRT08 ) 
+                F3 = FINDC( CHK06, TXCNT( 6 ), CHRT06 ) 
+                F2 = FINDC( CHK05, TXCNT( 5 ), CHRT05 ) 
+                F1 = FINDC( TSCC , TXCNT( 3 ), CHRT03 ) 
+                F0 = FINDC( TSCCL, TXCNT( 2 ), CHRT02 )
+
+C................. Check for mobile-specific matches that use a TSCC with
+C                  road class of zero and vehicle type. The assignment of
+C                  temporal profile based on  a vehicle type and no road class
+C                  comes after the road class only match (or TSCCL in CHRT08,
+C                  for example) but the match uses the full TSCC (or CHRT09, for
+C                  example).
+                IF( CATEGORY .EQ. 'MOBILE' ) THEN
+                    F4B = FINDC( CHK08B, TXCNT( 9 ), CHRT09 )
+                    F2B = FINDC( CHK05B, TXCNT( 6 ), CHRT06 )
+                    F0B = FINDC( CHK02B, TXCNT( 3 ), CHRT03 )
+                END IF
 
                 IF( F5 .GT. 0 .AND. IEFS09(F5,V) .GE. ADDPS ) THEN
                     EREF = IEFS09( F5,V ) - ADDPS
@@ -280,6 +302,11 @@ C                           activity-specific left SCC match
 
                 ELSEIF( F4 .GT. 0 .AND. IEFS08(F4,V) .GE. ADDPS ) THEN
                     EREF = IEFS08( F4,V ) - ADDPS
+                    CALL SETSOURCE_EFS
+                    CYCLE                       !  to end of sources-loop
+
+                ELSEIF( F4B .GT. 0 .AND. IEFS08(F4B,V) .GE. ADDPS ) THEN
+                    EREF = IEFS08( F4B,V ) - ADDPS
                     CALL SETSOURCE_EFS
                     CYCLE                       !  to end of sources-loop
 
@@ -293,6 +320,11 @@ C                           activity-specific left SCC match
                     CALL SETSOURCE_EFS
                     CYCLE                       !  to end of sources-loop
 
+                ELSEIF( F2B .GT. 0 .AND. IEFS05(F2B,V) .GE. ADDPS ) THEN
+                    EREF = IEFS05( F2B,V ) - ADDPS
+                    CALL SETSOURCE_EFS
+                    CYCLE                       !  to end of sources-loop
+
                 ELSEIF( F1 .GT. 0 .AND. IEFS03(F1,V) .GE. ADDPS ) THEN
                     EREF = IEFS03( F1,V ) - ADDPS
                     CALL SETSOURCE_EFS
@@ -300,6 +332,11 @@ C                           activity-specific left SCC match
 
                 ELSEIF( F0 .GT. 0 .AND. IEFS02(F0,V) .GE. ADDPS ) THEN
                     EREF = IEFS02( F0,V ) - ADDPS
+                    CALL SETSOURCE_EFS
+                    CYCLE                       !  to end of sources-loop
+
+                ELSEIF( F0B .GT. 0 .AND. IEFS03(F0B,V) .GE. ADDPS ) THEN
+                    EREF = IEFS03( F0B,V ) - ADDPS
                     CALL SETSOURCE_EFS
                     CYCLE                       !  to end of sources-loop
 
