ed28e39b44f676b84948ce02181f8c904cd1d0c6
mhouyoux <>
2001-02-06 18:11:38 +0000

    Added biogenic gridded output file

--- src/smkmerge/mrgonams.f
+++ src/smkmerge/mrgonams.f
@@ -67,6 +67,7 @@ C.........  Initialize - everything will be gridded
         TREPNAME = 'REPEG'
 
         AONAME = 'AG'
+        BONAME = 'BGTS'
         MONAME = 'MG'
         PONAME = 'PG'
         TONAME = 'EG'
@@ -181,6 +182,7 @@ C.............  Get output file names depending on if there are moles in units
                 CALL TRIM_AND_CONCAT( TREPNAME, '_L' )
 
                 CALL TRIM_AND_CONCAT( AONAME, '_L' )
+                CALL TRIM_AND_CONCAT( BONAME, '_L' )
                 CALL TRIM_AND_CONCAT( MONAME, '_L' )
                 CALL TRIM_AND_CONCAT( PONAME, '_L' )
                 CALL TRIM_AND_CONCAT( TONAME, '_L' )
@@ -196,6 +198,7 @@ C.............  Get output file names depending on if there are moles in units
                 CALL TRIM_AND_CONCAT( TREPNAME, '_S' )
 
                 CALL TRIM_AND_CONCAT( AONAME, '_S' )
+                CALL TRIM_AND_CONCAT( BONAME, '_S' )
                 CALL TRIM_AND_CONCAT( MONAME, '_S' )
                 CALL TRIM_AND_CONCAT( PONAME, '_S' )
                 CALL TRIM_AND_CONCAT( TONAME, '_S' )
