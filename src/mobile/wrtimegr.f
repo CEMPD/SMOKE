96b462923545427e29fa13a9e76cb787f13ff5bf
Catherine Seppanen <cseppan@email.unc.edu>
2001-10-24 14:43:31 +0000

    Writes all files with one call rather than separate calls.

--- src/mobile/wrtimegr.f
+++ src/mobile/wrtimegr.f
@@ -1,5 +1,5 @@
 
-        SUBROUTINE WRTIMEGR( TDEV, PERIOD )
+        SUBROUTINE WRTIMEGR
 
 C.........  MODULES for public variables
 C.........  This module contains the inventory arrays
@@ -15,13 +15,26 @@ C.........  This module contains the information about the source category
 C...........   INCLUDES:
 
         INCLUDE 'EMCNST3.EXT'   !  emissions constant parameters
-        
-C...........   SUBROUTINE ARGUMENTS
-        INTEGER, INTENT (IN) :: TDEV     ! time grouping file no.
-        INTEGER, INTENT (IN) :: PERIOD   ! period to write file for (daily, weekly, etc.)
 
+C...........   EXTERNAL FUNCTIONS and their descriptions:
+
+        INTEGER, EXTERNAL :: PROMPTFFILE
+        
 C...........   Other local variables         
         INTEGER I, J                      ! counters and indices
+        INTEGER CURRTIME                  ! current time period
+        INTEGER LASAFLAG                  ! treat local as arterial setting
+
+        INTEGER CURRDEV                   ! current file unit no.
+        INTEGER DAYDEV                    ! unit number for daily time period group file 
+        INTEGER WEEKDEV                   ! unit number for weekly group file
+        INTEGER MONTHDEV                  ! unit number for monthly group file
+        INTEGER EPISDEV                   ! unit number for episode length time period group
+        
+        LOGICAL :: OPENDAY   = .FALSE.    ! true: opened day group file
+        LOGICAL :: OPENWEEK  = .FALSE.    ! true: opened week group file
+        LOGICAL :: OPENMONTH = .FALSE.    ! true: opened month group file
+        LOGICAL :: OPENEPIS  = .FALSE.    ! true: opened episode length group file
 
         CHARACTER(LEN=FIPLEN3) REFCOUNTY                 ! ref. county FIPS code
         CHARACTER(LEN=FIPLEN3) INVCOUNTY                 ! inv. county FIPS code
@@ -36,45 +49,99 @@ C.........  Loop through ref. counties in MVREFSORT
             WRITE( REFCOUNTY, '(I6)' ) MVREFSORT( I,1 )
             CALL PADZERO( REFCOUNTY )
 
-C.............  Check if time period for current ref. county matches the one
-C               we are currently processing            
-            IF( MVREFSORT( I,3 ) == PERIOD ) THEN
-            	
-C.................  Check if this ref. county is not spatially averaged            	
-                IF( MVREFSORT( I,2 ) == 1 ) THEN                 
+C.............  Get time period for current ref. county
+            CURRTIME = MVREFSORT( I,3 )
 
-C.....................  Loop through inv. counties using this ref. county
-                    J = MCREFIDX( I,2 )
-
-                    DO
-                        WRITE( INVCOUNTY, '(I6)' ) MCREFSORT( J,1 )
-                        CALL PADZERO( INVCOUNTY )
-                        
-                        WRITE( TDEV,93010 ) INVCOUNTY, REFCOUNTY
-                        
-                        J = J + 1
-                        
-                        IF( J > NINVC ) EXIT
-                        IF( I /= NREFC ) THEN
-                            IF( J == MCREFIDX( I + 1,2 ) ) EXIT
-                        END IF
-
-                    END DO
-
-C.................  Otherwise, write the ref. county number
-                ELSE
-                    WRITE( TDEV,93010 ) REFCOUNTY, REFCOUNTY
+C.............  Set file unit based on current time period
+C               Open appropriate files if necessary            
+            SELECT CASE( CURRTIME )
+            
+            CASE( DAILY )
+            	IF( .NOT. OPENDAY ) THEN
+            	    DAYDEV = PROMPTFFILE(
+     &                       'Enter logical name for DAILYGROUP file',
+     &                       .FALSE., .TRUE., 'DAILYGROUP', PROGNAME )
+                    OPENDAY = .TRUE.
                 END IF
+                
+                CURRDEV = DAYDEV
+                
+            CASE( WEEKLY )
+                IF( .NOT. OPENWEEK ) THEN
+                    WEEKDEV = PROMPTFFILE(
+     &                        'Enter logical name for WEEKLYGROUP file',
+     &                        .FALSE., .TRUE., 'WEEKLYGROUP', PROGNAME )
+                    OPENWEEK = .TRUE.
+                END IF
+                
+                CURRDEV = WEEKDEV
+                
+            CASE( MONTHLY )
+                IF( .NOT. OPENMONTH ) THEN
+                    MONTHDEV = PROMPTFFILE(
+     &                       'Enter logical name for MONTHLYGROUP file',
+     &                       .FALSE., .TRUE., 'MONTHLYGROUP', PROGNAME )
+                    OPENMONTH = .TRUE.
+                END IF
+                
+                CURRDEV = MONTHDEV
+                
+            CASE( EPISLEN )
+                IF( .NOT. OPENEPIS ) THEN
+                    EPISDEV = PROMPTFFILE(
+     &                       'Enter logical name for EPISODEGROUP file',
+     &                       .FALSE., .TRUE., 'EPISODEGROUP', PROGNAME )
+                    OPENEPIS = .TRUE.
+            	END IF
+            	
+            	CURRDEV = EPISDEV
+            	
+            END SELECT
+
+C.............  Get local-to-arterial setting for this ref. county
+            LASAFLAG = MVREFSORT( I,4 )
+        	
+C.............  Check if this ref. county is not spatially averaged            	
+            IF( MVREFSORT( I,2 ) == 1 ) THEN
+
+C.................  Loop through inv. counties using this ref. county
+                J = MCREFIDX( I,2 )
+
+                DO
+                    WRITE( INVCOUNTY, '(I6)' ) MCREFSORT( J,1 )
+                    CALL PADZERO( INVCOUNTY )
                     
-            END IF ! end time period check
+                    WRITE( CURRDEV,93010 ) 
+     &                     INVCOUNTY, REFCOUNTY, LASAFLAG
+                    
+                    J = J + 1
+                    
+                    IF( J > NINVC ) EXIT
+                    IF( I /= NREFC ) THEN
+                        IF( J == MCREFIDX( I + 1,2 ) ) EXIT
+                    END IF
+
+                END DO
+
+C.............  Otherwise, write the ref. county number
+            ELSE
+                WRITE( CURRDEV,93010 ) 
+     &                 REFCOUNTY, REFCOUNTY, LASAFLAG
+            END IF
+                    
         END DO ! end ref. county loop
+
+        IF( OPENDAY )   CLOSE( DAYDEV )
+        IF( OPENWEEK )  CLOSE( WEEKDEV )
+        IF( OPENMONTH ) CLOSE( MONTHDEV )
+        IF( OPENEPIS )  CLOSE( EPISDEV )
 
 C******************  FORMAT  STATEMENTS   ******************************
 
 C...........   Formatted file I/O formats............ 93xxx
 
 93000   FORMAT( A )  
-93010   FORMAT( A6, 1X, A6 )  
+93010   FORMAT( A6, 1X, A6, 1X, I1 )  
         
         END SUBROUTINE WRTIMEGR
         
