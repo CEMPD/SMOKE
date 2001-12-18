4329d533efff31e498dc5d873f3ce441138d78e2
mhouyoux <>
2001-12-18 00:26:06 +0000

    Update to support speciation profiles being included in report

--- src/lib/prclinrc.f
+++ src/lib/prclinrc.f
@@ -28,7 +28,7 @@ C Project Title: Sparse Matrix Operator Kernel Emissions (SMOKE) Modeling
 C                System
 C File: @(#)$Id$
 C  
-C COPYRIGHT (C) 2000, MCNC--North Carolina Supercomputing Center
+C COPYRIGHT (C) 2001, MCNC--North Carolina Supercomputing Center
 C All Rights Reserved
 C  
 C See file COPYRIGHT for conditions of use.
@@ -626,6 +626,11 @@ C.............  BY options affecting inputs needed
      &                ( SEGMENT( 3 ) .EQ. 'STACKPARM' .OR.
      &                  SEGMENT( 4 ) .EQ. 'STACKPARM'     ) )
      &                  RPT_%STKPARM = .TRUE.
+
+                CASE( 'SPCCODE' )
+                    PSFLAG = .TRUE.
+                    RPT_%BYSPC = .TRUE.
+                    RPT_%SPCPOL = SEGMENT( 3 )
 
                 CASE( 'SRGCODE' )
                     IF( CATEGORY .NE. 'POINT' ) THEN
