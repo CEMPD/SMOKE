1dab946b2bd08743a63411c3fdfc131dcb243eae
mhouyoux <>
2002-01-07 16:49:58 +0000

    Updates to read temporal supplementation file and speciation supplemental file

--- src/emqa/rdrepin.f
+++ src/emqa/rdrepin.f
@@ -1,7 +1,7 @@
 
-        SUBROUTINE RDREPIN( GDIM, NSLIN, NSSIN, GDEV, SDEV, EDEV, YDEV, 
-     &                      NDEV, ENAME, GNAME, LNAME, SLNAME, SSNAME, 
-     &                      NX, IX, CX, SSMAT, SLMAT )
+        SUBROUTINE RDREPIN( GDIM, NSLIN, NSSIN, SDEV, GDEV, PDEV, TDEV,
+     &                      EDEV, YDEV, NDEV, ENAME, GNAME, LNAME, 
+     &                      SLNAME, SSNAME, NX, IX, CX, SSMAT, SLMAT )
 
 C***********************************************************************
 C  subroutine body starts at line 
@@ -24,7 +24,7 @@ C Project Title: Sparse Matrix Operator Kernel Emissions (SMOKE) Modeling
 C                System
 C File: @(#)$Id$
 C  
-C COPYRIGHT (C) 2000, MCNC--North Carolina Supercomputing Center
+C COPYRIGHT (C) 2001, MCNC--North Carolina Supercomputing Center
 C All Rights Reserved
 C  
 C See file COPYRIGHT for conditions of use.
@@ -70,14 +70,18 @@ C...........   INCLUDES
 
 C...........  EXTERNAL FUNCTIONS and their descriptions:
         INTEGER    GETFLINE
-        EXTERNAL   GETFLINE
+        INTEGER    INDEX1
+
+        EXTERNAL   GETFLINE, INDEX1
 
 C...........   SUBROUTINE ARGUMENTS
         INTEGER     , INTENT (IN) :: GDIM   ! no. mass spec input vars
         INTEGER     , INTENT (IN) :: NSLIN  ! no. mass spec input vars
         INTEGER     , INTENT (IN) :: NSSIN  ! no. mass spec input vars
-        INTEGER     , INTENT (IN) :: GDEV   ! unit no.: gridding supplemental
         INTEGER     , INTENT (IN) :: SDEV   ! unit no.: ASCII inven file
+        INTEGER     , INTENT (IN) :: GDEV   ! unit no.: gridding supplemental
+        INTEGER     , INTENT (IN) :: PDEV   ! unit no.: speciation supplemental
+        INTEGER     , INTENT (IN) :: TDEV   ! unit no.: temporal supplemental
         INTEGER     , INTENT (IN) :: EDEV   ! unit no.: elevated ID file (PELV)
         INTEGER     , INTENT (IN) :: YDEV   ! unit no.: cy/st/co file
         INTEGER     , INTENT (IN) :: NDEV   ! unit no.: SCC descriptions
@@ -93,7 +97,8 @@ C...........   SUBROUTINE ARGUMENTS
         REAL        , INTENT(OUT) :: SSMAT( NSRC, NSSIN ) ! mass spec coefs
  
 C.........  Local allocatable arrays
-        REAL, ALLOCATABLE :: LFRAC1L( : )  ! 1st-layer fraction
+        INTEGER, ALLOCATABLE :: IBUF   ( : )  ! tmp var for temporal profiles
+        REAL   , ALLOCATABLE :: LFRAC1L( : )  ! 1st-layer fraction
 
 C.........  Array that contains the names of the inventory variables needed for
 C           this program
@@ -105,21 +110,26 @@ C...........   Local variables that depend on module variables
 C...........   Other local variables
         INTEGER          I, J, K, L, L1, L2, N, V, S, T ! counters and indices
 
+        INTEGER          DIU                ! tmp diurnal profile number
         INTEGER          IOS                ! i/o status
         INTEGER          IREC               ! tmp record number
         INTEGER       :: JDATE = 0          ! Julian date
         INTEGER       :: JTIME = 0          ! time (HHMMSS)
+        INTEGER          MON                ! tmp monthly profile number
         INTEGER       :: NINVARR = 0        ! no. actual inventory inputs
+        INTEGER          NV                 ! tmp no. variables in temporal suplm
         INTEGER       :: SRGID1             ! tmp primary surrogate IDs
         INTEGER       :: SRGID2             ! tmp fallback surrogate IDs
+        INTEGER          WEK                ! tmp weekly profile number
 
         LOGICAL       :: LRDREGN = .FALSE.  !  true: read region code
         LOGICAL       :: EFLAG   = .FALSE.  !  true: error found
 
-         
+        CHARACTER*1            TTYP         !  temporal profile entry type
         CHARACTER*16  ::       BNAME = ' '  !  name buffer
         CHARACTER*50           BUFFER       !  string buffer
         CHARACTER*300          MESG         !  message buffer
+        CHARACTER(LEN=IOVLEN3) CBUF         !  tmp pollutant name
         CHARACTER(LEN=IODLEN3) DBUF         !  tmp variable name
         CHARACTER(LEN=SRCLEN3) CSRC         !  tmp source chars
 
@@ -264,6 +274,149 @@ C.............  Get file header for variable names
 
         END IF
 
+C.........  If needed, read in speciation supplementation matrix
+        IF( PSFLAG ) THEN
+
+            ALLOCATE( SPPROF( NSRC,NSPCPOL ), STAT=IOS )
+            CALL CHECKMEM( IOS, 'SPPROF', PROGNAME )
+            SPPROF = ' '
+
+            MESG = 'supplemental speciation file'
+            N = GETFLINE( PDEV, MESG )
+
+            IREC = 0
+            
+            DO I = 1, N
+
+                READ( PDEV, '(A)', END=999, IOSTAT=IOS ) BUFFER
+                IREC = IREC + 1
+
+                IF ( IOS .NE. 0 ) THEN
+                    EFLAG = .TRUE.
+                    WRITE( MESG,94010 ) 
+     &                'I/O error', IOS, 'reading supplemental ' //
+     &                'speciation file at line', IREC
+                    CALL M3MESG( MESG )
+                    CYCLE
+                END IF
+
+C.................  See if this line is a pollutant name
+                L1 = INDEX( BUFFER, '"' )        ! Find start quote
+
+C.................  If pollutant name, figure out which pollutant index and
+C                   reset source counter to 0.
+                IF ( L1 .GT. 0 ) THEN
+
+                    L2 = LEN_TRIM( BUFFER )     ! Find end quote
+                    CBUF = BUFFER( L1+1:L2-1 )  
+
+C.....................  Check if this pollutant is one selected for reporting 
+                    V = INDEX1( CBUF, NSPCPOL, SPCPOL )
+                    IF ( V .GT. 0 ) THEN
+                        LSPCPOL( V ) = .TRUE.
+                        S = 0
+                    END IF
+                        
+C.................  If not pollutant name, then continue to read in the 
+C                   pollutant codes and store them by source
+                ELSE IF ( V .GT. 0 ) THEN
+                    S = S + 1
+
+                    BUFFER = ADJUSTL( BUFFER )
+                    SPPROF( S,V ) = ADJUSTR( BUFFER( 1:SPNLEN3 ) )
+
+C.....................  Handle case where spaces have been added to file.
+                    IF ( S .EQ. NSRC ) V = 0
+
+                END IF
+                
+            END DO            
+
+        END IF
+
+C.........  If needed, read in temporal supplementation matrix
+        IF( TSFLAG ) THEN
+
+            ALLOCATE( IDIU( NSRC ), STAT=IOS )
+            CALL CHECKMEM( IOS, 'IDIU', PROGNAME )
+            ALLOCATE( IWEK( NSRC ), STAT=IOS )
+            CALL CHECKMEM( IOS, 'IWEK', PROGNAME )
+            ALLOCATE( IMON( NSRC ), STAT=IOS )
+            CALL CHECKMEM( IOS, 'IMON', PROGNAME )
+            ALLOCATE( IBUF( NIPPA ), STAT=IOS )
+            CALL CHECKMEM( IOS, 'IBUF', PROGNAME )
+            IDIU = 0
+            IWEK = 0
+            IMON = 0
+            IBUF = 0
+
+            MESG = 'Supplemental temporal file'
+            N = GETFLINE( TDEV, MESG )
+
+C.............  Check file format, assuming that pollutants weren't processed
+C               in > 1 groups.  (this routine doesn't handle grouped processing)
+            IF ( ( (N-1)/3 ) .NE. NSRC ) THEN
+                MESG = 'INTERNAL ERROR: ' // CRL// 'TSUP file has '//
+     &                 'inconsistent number of lines with NSRC'
+                CALL M3MSG2( MESG )
+                CALL M3EXIT( PROGNAME, 0, 0, ' ', 2 )
+            END IF
+
+C.............  Skip file header
+            READ( TDEV, * )
+
+            IREC = 1
+            DO I = 2, N
+
+                READ( TDEV, *, END=999, IOSTAT=IOS ) 
+     &              TTYP, NV, ( IBUF( V ), V=1, NV )
+                IREC = IREC + 1
+
+                IF ( IOS .NE. 0 ) THEN
+                    EFLAG = .TRUE.
+                    WRITE( MESG,94010 ) 
+     &                'I/O error', IOS, 
+     &                'reading supplemental temporal file at line', IREC
+                    CALL M3MESG( MESG )
+                    CYCLE
+                END IF
+
+C.................  NV > 1 is not supported
+                IF ( NV .GT. 1 ) THEN
+                    MESG = 'Number of variables in ' // CRL //
+     &                     'TSUP file is not supported.'
+                    CALL M3EXIT( PROGNAME, 0, 0, MESG, 2 )
+                END IF
+
+                S = INT( ( I-2 ) / 3 ) + 1
+                SELECT CASE( TTYP )
+                CASE ( 'M' )
+                    IF ( NV .EQ. 1 ) THEN
+                        IMON( S ) = IBUF( 1 )
+                    ELSE
+c note: this is not ready yet
+                    ENDIF
+
+                CASE ( 'W' )
+                    IF ( NV .EQ. 1 ) THEN
+                        IWEK( S ) = IBUF( 1 )
+                    ELSE
+c note: this is not ready yet
+                    ENDIF
+
+                CASE ( 'H' )
+                    IF ( NV .EQ. 1 ) THEN
+                        IDIU( S ) = IBUF( 1 )
+                    ELSE
+c note: this is not ready yet
+                    ENDIF
+
+                END SELECT
+              
+            END DO            
+
+        END IF
+
 C.........  If needed, read in country, state, county file
         IF( YFLAG ) THEN
             CALL RDSTCY( YDEV, NINVIFIP, INVIFIP )
@@ -275,10 +428,7 @@ C.........  If needed, read in elevated source indentification file
         END IF
 
 C.........  If needed, read in SCC descriptions file
-        IF( NFLAG ) THEN
-c           CALL RDSCCDSC( NDEV, NINVSCC, INVSCC )
-c note: still must write this
-        END IF
+        IF( NFLAG ) CALL RDSCCDSC( NDEV )
 
 C.........  If needed, read in layer fractions file to identify elevated
 C           sources
