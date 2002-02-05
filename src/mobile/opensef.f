16cb6250525727b8453e839b5efac2836c8aaeda
Catherine Seppanen <cseppan@email.unc.edu>
2002-02-05 16:36:21 +0000

    Automatically creates next file in requested directory.

--- src/mobile/opensef.f
+++ src/mobile/opensef.f
@@ -1,5 +1,5 @@
 
-        SUBROUTINE OPENSEF( SRCCT, SDATE, STIME, FNAME )
+        SUBROUTINE OPENSEF( SRCCT, DESC, SDATE, STIME, EMISDIR, FNAME )
 
 C...........   MODULES for public variables
 C...........   This module contains the information about the source category
@@ -20,13 +20,16 @@ C...........   EXTERNAL FUNCTIONS and their descriptions:
         CHARACTER(LEN=IODLEN3) GETCFDSC
         CHARACTER*16           PROMPTMFILE
         CHARACTER*16           VERCHAR
+        LOGICAL                OPNFULL3
 
-        EXTERNAL        GETCFDSC, PROMPTMFILE, VERCHAR
+        EXTERNAL        GETCFDSC, PROMPTMFILE, VERCHAR, OPNFULL3
 
 C...........   SUBROUTINE ARGUMENTS
         INTEGER     , INTENT    (IN) :: SRCCT    ! total number of sources
+        CHARACTER(*), INTENT    (IN) :: DESC     ! description of file type
         INTEGER     , INTENT    (IN) :: SDATE    ! julian start date
         INTEGER     , INTENT    (IN) :: STIME    ! HHMMSS start time
+        CHARACTER(*), INTENT    (IN) :: EMISDIR  ! directory of output files
         CHARACTER(*), INTENT(IN OUT) :: FNAME    ! name output emission factors file
 
 C...........   LOCAL PARAMETERS
@@ -36,6 +39,7 @@ C...........   Other local variables
         INTEGER     J
 
         CHARACTER*300   MESG    !  message buffer
+        CHARACTER*256   FULLNAME ! full file name
 
         CHARACTER*16 :: PROGNAME = 'OPENSEF' ! program name
 
@@ -59,6 +63,7 @@ C.........  Set header values that cannot be default
         TSTEP3D = 10000
         NVARS3D = NEFS + 1
         NROWS3D = SRCCT
+        NLAYS3D = 1
  
         J = 1
         VNAME3D( J ) = 'SOURCES'
@@ -73,10 +78,16 @@ C.........  Set header values that cannot be default
             VTYPE3D( J ) = M3REAL
         END DO
 
-C.........  Prompt for source-based output file
-        FNAME = PROMPTMFILE(
-     &          'Enter logical name for EMISSION FACTORS file',
-     &          FSUNKN3, FNAME, PROGNAME )
+C.........  Create full file name
+        WRITE( FULLNAME,94010 ) EMISDIR( 1:LEN_TRIM( EMISDIR ) ) //
+     &                          '/emisfacs.' // DESC // '.', SDATE,
+     &                          '.ncf' 
+
+C.........  Open new file
+        IF( .NOT. OPNFULL3( FNAME, FSNEW3, FULLNAME, PROGNAME ) ) THEN
+            MESG = 'Could not create new output file ' // FULLNAME
+            CALL M3EXIT( PROGNAME, 0, 0, MESG, 2 )
+        END IF
 
         RETURN
 
@@ -84,7 +95,7 @@ C******************  FORMAT  STATEMENTS   ******************************
 
 C...........   Internal buffering formats............ 94xxx
 
-94010   FORMAT( 10( A, :, I8, :, 1X ) )
+94010   FORMAT( A, I7, A )
 
 94030   FORMAT( A, F15.9, 1X, A )
 
