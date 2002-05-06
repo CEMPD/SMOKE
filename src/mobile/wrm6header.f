7d702a7f82d72bbc2cf37fcc56d2c6c7e4c65124
Catherine Seppanen <cseppan@email.unc.edu>
2002-05-06 15:45:25 +0000

    Writes pollutant commands to M6 input file based on info read from MEPROC file

--- src/mobile/wrm6header.f
+++ src/mobile/wrm6header.f
@@ -1,20 +1,123 @@
 
         SUBROUTINE WRM6HEADER( MDEV )
+
+C...........   This module contains emission factor tables and related
+        USE MODEMFAC, ONLY: EMTPOL, NEPOL
         
         IMPLICIT NONE
         
 C...........   SUBROUTINE ARGUMENTS
         INTEGER, INTENT (IN) :: MDEV    ! M6 input file unit no.        
 
+C...........   Local variables
+        INTEGER           I             ! counter
+
+        LOGICAL           WROTE_OCARB   ! true: wrote OCARBON to pol array
+        LOGICAL           WROTE_ECARB   ! true: wrote ECARBON to pol array
+        LOGICAL           WROTE_GASPM   ! true: wrote GASPM to pol array
+        LOGICAL           WROTE_BRAKE   ! true: wrote BRAKE to pol array
+        LOGICAL           WROTE_TIRE    ! true: wrote TIRE to pol array
+
+        CHARACTER(LEN=50)  BASICPOL     ! basic pollutants to calculate
+        CHARACTER(LEN=50)  PMPOL        ! particulates to calculate
+        CHARACTER(LEN=50)  AIRPOL       ! air toxics to calculate
+        CHARACTER(LEN=300) MESG         ! message buffer 
+
 C***********************************************************************
 C   begin body of subroutine WRM6HEADER
 
+C.........  Initialize arrays and logical variables
+        BASICPOL = ' '
+        PMPOL    = ' '
+        AIRPOL   = ' '
+        
+        WROTE_OCARB = .FALSE.
+        WROTE_ECARB = .FALSE.
+        WROTE_GASPM = .FALSE.
+        WROTE_BRAKE = .FALSE.
+        WROTE_TIRE  = .FALSE.
+
+C.........  Write basic MOBILE6 header info
         WRITE( MDEV,93000 ) 'MOBILE6 INPUT FILE :'
         WRITE( MDEV,93000 ) 'NO DESC OUTPUT     :'
         WRITE( MDEV,93000 ) 'DATABASE OUTPUT    :'
-        WRITE( MDEV,93000 ) 'POLLUTANTS         : CO NOX HC'
-        WRITE( MDEV,93000 ) 'PARTICULATES       :'
-        WRITE( MDEV,93000 ) 'AIR TOXICS         :'
+
+C.........  Loop through pollutants and generate MOBILE6 inputs        
+        DO I = 1, NEPOL
+            SELECT CASE ( EMTPOL( I ) )
+            CASE( 'CO' )
+                BASICPOL = TRIM( BASICPOL ) // ' CO'
+            CASE( 'NOX' )
+                BASICPOL = TRIM( BASICPOL ) // ' NOX'
+            CASE( 'VOC', 'THC', 'NMH', 'TOG', 'NMO', 'ROG' )
+                BASICPOL = TRIM( BASICPOL ) // ' HC'
+            CASE( 'SO4' )
+                PMPOL = TRIM( PMPOL ) // ' SO4'
+            CASE( 'OCARB25', 'OCARBPMC' )
+                IF( .NOT. WROTE_OCARB ) THEN
+                    PMPOL = TRIM( PMPOL ) // ' OCARBON'
+                    WROTE_OCARB = .TRUE.
+                END IF
+            CASE( 'ECARB25', 'ECARBPMC' )
+                IF( .NOT. WROTE_ECARB ) THEN
+                    PMPOL = TRIM( PMPOL ) // ' ECARBON'
+                    WROTE_ECARB = .TRUE.
+                END IF
+            CASE( 'GASPM25', 'GASPMC' )
+                IF( .NOT. WROTE_GASPM ) THEN
+                    PMPOL = TRIM( PMPOL ) // ' GASPM'
+                    WROTE_GASPM = .TRUE.
+                END IF
+            CASE( 'SO2' )
+                PMPOL = TRIM( PMPOL ) // ' SO2'
+            CASE( 'NH3' )
+                PMPOL = TRIM( PMPOL ) // ' NH3'
+            CASE( 'BRAKE25', 'BRAKEPMC' )
+                IF( .NOT. WROTE_BRAKE ) THEN
+                    PMPOL = TRIM( PMPOL ) // ' BRAKE'
+                    WROTE_BRAKE = .TRUE.
+                END IF
+            CASE( 'TIRE25', 'TIREPMC' )
+                IF( .NOT. WROTE_TIRE ) THEN
+                    PMPOL = TRIM( PMPOL ) // ' TIRE'
+                    WROTE_TIRE = .TRUE.
+                END IF
+            CASE( 'BEN' )
+                AIRPOL = TRIM( AIRPOL ) // ' BENZ'
+            CASE( 'MTB' )
+                AIRPOL = TRIM( AIRPOL ) // ' MTBE'
+            CASE( 'BUT' )
+                AIRPOL = TRIM( AIRPOL ) // ' BUTA'
+            CASE( 'FOR' )
+                AIRPOL = TRIM( AIRPOL ) // ' FORM'
+            CASE( 'ACE' )
+                AIRPOL = TRIM( AIRPOL ) // ' ACETA'
+            CASE( 'ACR' )
+                AIRPOL = TRIM( AIRPOL ) // ' ACROL'
+            CASE DEFAULT
+                MESG = 'WARNING: Unknown pollutant ' // 
+     &                 TRIM( EMTPOL( I ) ) // ' in MEPROC file. ' //
+     &                 'Pollutant will be ignored.'
+                CALL M3MESG( MESG )
+            END SELECT
+        END DO
+
+        IF( BASICPOL /= ' ' ) THEN
+            WRITE( MDEV,93000 ) 'POLLUTANTS         : ' // 
+     &                          TRIM( BASICPOL )
+        END IF
+        
+        IF( PMPOL /= ' ' ) THEN
+            WRITE( MDEV,93000 ) 'PARTICULATES       : ' // 
+     &                          TRIM( PMPOL )
+        END IF
+        
+        IF( AIRPOL /= ' ' ) THEN
+            WRITE( MDEV,93000 ) 'AIR TOXICS         : ' // 
+     &                          TRIM( AIRPOL )
+        END IF
+
+C.........  Finish MOBILE6 header
         WRITE( MDEV,93000 ) 'RUN DATA           :'
         WRITE( MDEV,93000 ) ' '
 
