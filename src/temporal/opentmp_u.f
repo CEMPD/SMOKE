a1d019e8b9d3811801375e1e66e3a3edf6ae29f5
gcano <>
2002-08-08 17:11:18 +0000

    manages the output file for uncertainty emissions. the logical name A/P TMPU is reused here.

--- src/temporal/opentmp_u.f
+++ src/temporal/opentmp_u.f
@@ -1,6 +1,6 @@
 
         SUBROUTINE OPENTMP_U( ENAME, UINAME,SDATE, STIME, TSTEP, TZONE,
-     &                        RLZN, MAXLEN, UNAME, UDEV )
+     &                        RLZN, UNAME, UDEV )
 
 C***********************************************************************
 C  subroutine body starts at line 103
@@ -67,9 +67,10 @@ C...........   EXTERNAL FUNCTIONS and their descriptions:
         INTEGER                PROMPTFFILE
         CHARACTER(LEN=NAMLEN3) PROMPTMFILE
         CHARACTER*16           VERCHAR
+        LOGICAL                SETENVVAR
 
         EXTERNAL        CRLF, INDEX1, GETCFDSC, GETIFDSC, MULTUNIT,
-     &                  PROMPTMFILE, VERCHAR
+     &                  PROMPTMFILE, VERCHAR, SETENVVAR
 
 C...........   SUBROUTINE ARGUMENTS
         CHARACTER(*), INTENT (IN) :: ENAME  ! emissions inven logical name
@@ -79,13 +80,11 @@ C...........   SUBROUTINE ARGUMENTS
         INTEGER     , INTENT (IN) :: TSTEP  ! episode time step
         INTEGER     , INTENT (IN) :: TZONE  ! zone used for hours in output files
         INTEGER     , INTENT (IN) :: RLZN   ! realization number
-        INTEGER     , INTENT (IN) :: MAXLEN ! the length of the number of realizations 
-        CHARACTER(*), INTENT(OUT) :: UNAME  ! lay-1 (or all) hourly logical name 
+        CHARACTER(*), INTENT(IN OUT) :: UNAME  ! lay-1 (or all) hourly logical name 
         INTEGER     , INTENT(OUT) :: UDEV   ! unit number of temporal supmtl file
 
 C...........   LOCAL PARAMETERS
         CHARACTER*50, PARAMETER :: CVSW = '$Name$'  ! CVS revision tag
-        CHARACTER*300              TMPBUF              ! temporay name label
 
 C...........   Other local variables
 
@@ -93,9 +92,12 @@ C...........   Other local variables
 
         INTEGER                      NINVVAR     ! number of inventory variables
         INTEGER                      PYEAR       ! projected year from inventory file (or -1)
-        INTEGER, SAVE             :: NLEN = 0    ! temporary string length
 
         CHARACTER*5     CTZONE                   ! string of time zone
+        CHARACTER*16    FNAM                     ! file name buffer 
+        CHARACTER*300   DIRBUF                   ! directory buffer 
+        CHARACTER*300   FNAMX                    ! file name extension 
+        CHARACTER*300   LNAME                    ! full path and name for uncertainty files
         CHARACTER*300   MESG                     ! message buffer 
 
         CHARACTER(LEN=NAMLEN3)  NAMBUF           ! file name buffer
@@ -106,84 +108,69 @@ C...........   Other local variables
 C***********************************************************************
 C   begin body of subroutine OPENTMP
 
-C.........  Write time zone to character string
-        WRITE( CTZONE,94000 ) TZONE
- 
+        IF( RLZN .EQ. 1) THEN
+
+            UNAME = UCAT( 1:1 ) // 'TMPU'
+
+        ELSE IF( RLZN .GT. 1) THEN
+            IF( .NOT.( CLOSE3( UNAME ) ) ) THEN
+                WRITE( MESG,94010 ) 
+     &                 'WARNING: Unable to close file' // UNAME
+                CALL M3MSG2( MESG )
+            END IF
+        END IF
+
 C.........  Set up file header(s) for opening I/O API output(s). Base this on
 C           inventory header...
+        CALL GRFNAME
+        IF( .NOT.( SETENVVAR( UNAME, LNAME ) ) )
+     &       CALL M3EXIT( PROGNAME, 0, 0, 'Unable to assign setenv', 2 )
 
-c BUG may need to add uncertainty file info gets
-C.........  Get header information from inventory file
-c        IF( .NOT. DESC3( UINAME ) ) THEN
-c            MESG = 'Could not get description of file "' 
-c     &             // ENAME( 1:LEN_TRIM( ENAME ) ) // '".'
-c            CALL M3EXIT( PROGNAME, 0, 0, MESG, 2 )
-c        END IF
+C.........  Write time zone to character string
+        WRITE( CTZONE,94000 ) TZONE
 
 C.........  Get header information from inventory file
-        IF( .NOT. DESC3( ENAME ) ) THEN
+        IF( .NOT. DESC3( UINAME ) ) THEN
             MESG = 'Could not get description of file "' 
-     &             // ENAME( 1:LEN_TRIM( ENAME ) ) // '".'
+     &             // UINAME( 1:LEN_TRIM( UINAME ) ) // '".'
             CALL M3EXIT( PROGNAME, 0, 0, MESG, 2 )
         END IF
 
-        IFDESC2 = GETCFDSC( FDESC3D, '/FROM/', .TRUE. )
-        IFDESC3 = GETCFDSC( FDESC3D, '/VERSION/', .TRUE. )
-        NINVVAR = NVARS3D
-        BYEAR   = GETIFDSC( FDESC3D, '/BASE YEAR/', .TRUE. )
-        PYEAR   = GETIFDSC( FDESC3D, '/PROJECTED YEAR/', .FALSE. )
+c        IFDESC2 = GETCFDSC( FDESC3D, '/FROM/', .TRUE. )
+c        IFDESC3 = GETCFDSC( FDESC3D, '/VERSION/', .TRUE. )
+c        NINVVAR = NVARS3D
+c        BYEAR   = GETIFDSC( FDESC3D, '/BASE YEAR/', .TRUE. )
+c        PYEAR   = GETIFDSC( FDESC3D, '/PROJECTED YEAR/', .FALSE. )
 
+        NLAYS3D = 1
+        NCOLS3D = 1
         NROWS3D = UNSRC
         NVARS3D = UNIPOL
         SDATE3D = SDATE
         STIME3D = STIME
         TSTEP3D = TSTEP
-
-        FDESC3D = ' '   ! array
-
+        FTYPE3D = GRDDED3
+c        GDNAM3D = ' '
+c        FDESC3D = ' '   ! array
+ 
         FDESC3D( 1 ) = CATDESC // ' source hourly emissions data'
         FDESC3D( 2 ) = '/FROM/ '    // PROGNAME
         FDESC3D( 3 ) = '/VERSION/ ' // VERCHAR( CVSW )
         FDESC3D( 4 ) = '/TZONE/ '   // CTZONE
-        WRITE( FDESC3D( 5 ),94010 ) '/BASE YEAR/ ', BYEAR 
-        IF( PYEAR .GT. 0 ) 
-     &      WRITE( FDESC3D( 6 ),94010 ) '/PROJECTED YEAR/ ', PYEAR
-	WRITE( FDESC3D( 7 ),94010 ) '/OZONE SEASON/', INVPIDX
-
-        FDESC3D( 11 ) = '/INVEN FROM/ ' // IFDESC2
-        FDESC3D( 12 ) = '/INVEN VERSION/ ' // IFDESC3
 
 C.........  Set variable names and characteristics from the emission types
         K = 0
-
-
-c        DO J = 1, NIACT
-C.............  Double check that pollutant is in the inventory file
-C.............  Use EAREAD, because for mobile sources, EANAM has been
-C               expanded to contain the emission types
-c            I = INDEX1( ACTVTY( J ), NIACT + NIPOL, EAREAD )
-c            IF( I .LE. 0 ) THEN
-c                MESG='INTERNAL ERROR: inventory file variables changed!'
-c                CALL M3MSG2( MESG )
-c                CALL M3EXIT( PROGNAME, 0, 0, ' ', 2 )
-c            END IF
-
-c            DO V = 1, NETYPE( I-NIPOL )
-
-c        	K = K + 1
-c       	VNAME3D( K ) = EMTNAM( V,J )
-c        	UNITS3D( K ) = EAUNIT( I )   
-c        	VDESC3D( K ) = EMTDSC( V,J )
-c        	VTYPE3D( K ) = M3REAL
-
-c            END DO  ! End loop on emission types for output
-c        END DO      ! End loop on activities output
 
 C.........  Set variable names and characteristics from the pollutants
         DO V = 1, NIPOL
 
 C.............  Double check that uncertainty pollutant is in the inventory file
             I = INDEX1( EINAM( V ), NIPPA, EANAM )
+            IF( I .LE. 0 ) THEN
+                MESG='INTERNAL ERROR: inventory file variables changed!'
+                CALL M3MSG2( MESG )
+                CALL M3EXIT( PROGNAME, 0, 0, ' ', 2 )
+            END IF
             IF( .NOT. ( UEINAM( I ) ) ) CYCLE
 
             K = K + 1
@@ -194,17 +181,17 @@ C.............  Double check that uncertainty pollutant is in the inventory file
 
         END DO  ! End loop on pollutants for output
 
-C.........  Set up name for output file
-        WRITE( TMPBUF,94010 ) '', RLZN
-        TMPBUF = ADJUSTL( TMPBUF )
-        IF (NLEN .EQ. 0) NLEN = MAXLEN + 1
-        CALL PADNZERO( NLEN, TMPBUF )
-        WRITE( UNAME,94010 ) CRL // 'TMP_U' // TMPBUF(1:NLEN)
-
 C.........  Prompt for and open I/O API output file(s)...
         MESG = 'Enter name for UNCERTAINTY output HOURLY EMISSIONS file'
-        NAMBUF = PROMPTMFILE( MESG, FSUNKN3, UNAME, PROGNAME )
+        NAMBUF = PROMPTMFILE( MESG, FSUNKN3, 
+     &                        UCAT( 1:1 ) // 'TMPU', PROGNAME )
         UNAME = NAMBUF
+
+C.........  Open supplemental speciation file
+c        MESG = 'Enter logical name for the TEMPORAL SUPPLEMENTAL '//
+c     &         'file'
+c        PDEV = PROMPTFFILE( MESG, .FALSE., .TRUE., 
+c     &                      CRL // 'TSUP', PROGNAME )
 
         RETURN
 
@@ -215,5 +202,35 @@ C...........   Internal buffering formats............ 94xxx
 94000   FORMAT( I2.2 )
  
 94010   FORMAT( 10( A, :, I8, :, 1X ) )
+
+        CONTAINS
+
+            SUBROUTINE GRFNAME
+
+            INTEGER         L1, L2, L3          ! temporary numbers
+
+            CHARACTER*300   TMPBUF              ! temporay name label
+C**********************************************************************
+
+C.........  Set up name for output file
+        CALL NAMEVAL( UCAT( 1:1 ) // 'TMPU_ODIR', DIRBUF )
+        L1 = LEN_TRIM( DIRBUF )
+        CALL NAMEVAL( UCAT( 1:1 ) // 'TMPU_FNAM', FNAM )
+        L2 = LEN_TRIM( FNAM )
+
+        WRITE( TMPBUF,94010 ) '', RLZN
+        TMPBUF = ADJUSTL( TMPBUF )
+        CALL PADNZERO( RMXLEN, TMPBUF )
+        L3 = LEN_TRIM( TMPBUF )
+
+        LNAME = DIRBUF( 1:L1 ) // '/' // 
+     &          FNAM( 1: L2 )  // '_' //
+     &          TMPBUF( 1: L3 )  // '.ncf' 
+
+
+C...........   Internal buffering formats............ 94xxx
+94010   FORMAT( 10( A, :, I8, :, 1X ) )
+
+            END SUBROUTINE GRFNAME
 
         END SUBROUTINE OPENTMP_U
