4d400b13bfe4e97fd9b00f9f518b183ecc8330e9
Catherine Seppanen <cseppan@email.unc.edu>
2003-04-25 13:02:48 +0000

    Changed IOAPI_GRID_SIZE to return with megabytes rather than bytes

--- src/temporal/opentmp.f
+++ src/temporal/opentmp.f
@@ -164,7 +164,7 @@ C.........  Check file size and adjust number of files to avoid 2 GB limit
             FILESIZE = IOAPI_GRD_SIZE( NCOLS3D, NROWS3D, NLAYS3D, 
      &                                 NVARFILE, NSTEPS )
             
-            IF( FILESIZE / 1000000 > 1500 ) THEN
+            IF( FILESIZE > 1500 ) THEN
                 NFILESET = NFILESET + 1
             ELSE
                 EXIT
