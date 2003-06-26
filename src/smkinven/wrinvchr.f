d4e90e94af6fd29af2bf32e44187dad4e3c74eca
Catherine Seppanen <cseppan@email.unc.edu>
2003-06-26 14:59:38 +0000

    Writes MACT, NAICS, source type code, and emission release point type to ASCII inventory file; writes ISIC to area source netCDF file

--- src/smkinven/wrinvchr.f
+++ src/smkinven/wrinvchr.f
@@ -1,5 +1,5 @@
 
-        SUBROUTINE WRINVCHR( ENAME, SDEV, A2PFLAG )
+        SUBROUTINE WRINVCHR( ENAME, SDEV, A2PFLAG, NONPOINT )
 
 C***********************************************************************
 C  subroutine body starts at line
@@ -60,13 +60,14 @@ C.........  SUBROUTINE ARGUMENTS and their descriptions:
         CHARACTER(*), INTENT (IN) :: ENAME   ! I/O API file name
         INTEGER     , INTENT (IN) :: SDEV    ! ASCII file unit
         LOGICAL     , INTENT (IN) :: A2PFLAG ! true: using area-to-point processing
+        LOGICAL     , INTENT (IN) :: NONPOINT! true: processing nonpoint inventory
 
 C.........  Arrays for column formatting
-        INTEGER       COLWID( MXCHRS+4 ) !  width of source info columns
+        INTEGER       COLWID( MXCHRS+8 ) !  width of source info columns
 
-        LOGICAL       LF    ( MXCHRS+4 ) !  true if column should be output
+        LOGICAL       LF    ( MXCHRS+8 ) !  true if column should be output
 
-        CHARACTER*300 CHARS ( MXCHRS+4 ) !  source fields for output
+        CHARACTER*300 CHARS ( MXCHRS+8 ) !  source fields for output
 
 C.........  Source-specific header arrays
         CHARACTER*20 :: ARHEADRS( MXARCHR3+1 ) = 
@@ -78,13 +79,13 @@ C.........  Source-specific header arrays
         CHARACTER*20 :: MBHEADRS( MXMBCHR3+2 ) = 
      &                                    ( / 'SMOKE Source ID     ',  
      &                                        'Cntry/St/Co FIPS    ',
-     &                                        'Roadway type code   ',
+     &                                        'Roadway Type code   ',
      &                                        'Link ID             ',
-     &                                        'Vehicle type code   ',
+     &                                        'Vehicle Type code   ',
      &                                        'SCC                 ', 
      &                                        'Vehicle Type Name   ' / ) 
 
-        CHARACTER*20 :: PTHEADRS( MXPTCHR3+5 ) = 
+        CHARACTER*20 :: PTHEADRS( MXPTCHR3+9 ) = 
      &                                    ( / 'SMOKE Source ID     ',  
      &                                        'Cntry/St/Co FIPS    ',
      &                                        'Plant code          ', 
@@ -95,7 +96,11 @@ C.........  Source-specific header arrays
      &                                        'Source char 5       ',
      &                                        'SCC                 ',  
      &                                        'DOE plant ID        ',  
-     &                                        'Boiler code         ',  
+     &                                        'Boiler code         ',
+     &                                        'MACT code           ',
+     &                                        'NAICS code          ',
+     &                                        'Source type code    ',
+     &                                        'Emission release pt ',  
      &                                        'Facility description' / )
 
 C.........  Allocatable header arrays
@@ -108,6 +113,7 @@ C.........  Other local variables
         INTEGER       IOS              !  memory allocation status
         INTEGER       L1, L2, NL       !  counters and indices
         INTEGER       M1, M2, M3, M4   !  positions after src characteristics
+        INTEGER       M5, M6, M7, M8
         INTEGER       NASCII           !  number of posible output fields
         INTEGER       NC               !  number of output fields
 
@@ -162,6 +168,11 @@ C.........  Write the I/O API file, one variable at a time
             
             IF ( .NOT. WRITESET( ENAME, 'CELLID', ALLFILES, 
      &                           0, 0, CELLID ) ) THEN
+                CALL M3EXIT( PROGNAME, 0, 0, MESG, 2 )
+            END IF
+
+            IF ( .NOT. WRITESET( ENAME, 'ISIC', ALLFILES, 
+     &                           0, 0, ISIC ) ) THEN
                 CALL M3EXIT( PROGNAME, 0, 0, MESG, 2 )
             END IF
 
@@ -243,10 +254,11 @@ C.........  Set the number of potential ASCII columns in SDEV output file
         SELECT CASE( CATEGORY )
         CASE( 'AREA' )
             NASCII = MXARCHR3
+            IF( NONPOINT ) NASCII = NASCII + 3
         CASE( 'MOBILE' )
             NASCII = MXMBCHR3+1
         CASE( 'POINT' )
-            NASCII = MXPTCHR3+4
+            NASCII = MXPTCHR3+8
         END SELECT
 
 C.........  Allocate memory for and populate the output header fields from
@@ -255,7 +267,12 @@ C           the source-specific fields
         CALL CHECKMEM( IOS, 'HDRFLDS', PROGNAME )
         SELECT CASE( CATEGORY )
         CASE( 'AREA' )
-            HDRFLDS = ARHEADRS  ! array
+            HDRFLDS( 1:4 ) = ARHEADRS  ! array
+            IF( NONPOINT ) THEN
+                HDRFLDS( NASCII+1-2 ) = 'MACT code           '
+                HDRFLDS( NASCII+1-1 ) = 'NAICS code          '
+                HDRFLDS( NASCII+1 )   = 'Source type code    '
+            END IF
         CASE( 'MOBILE' )
             HDRFLDS = MBHEADRS  ! array
         CASE( 'POINT' )
@@ -268,6 +285,10 @@ C           characteristics fields
         M2 = MXCHRS + 2
         M3 = MXCHRS + 3
         M4 = MXCHRS + 4
+        M5 = MXCHRS + 5
+        M6 = MXCHRS + 6
+        M7 = MXCHRS + 7
+        M8 = MXCHRS + 8
 
 C.........  Get the maximum column width for each of the columns in ASCII file
         COLWID = 0    ! array
@@ -283,6 +304,21 @@ C.........  Get the maximum column width for each of the columns in ASCII file
             END DO
 
             SELECT CASE ( CATEGORY )
+            CASE( 'AREA' )
+                IF( NONPOINT ) THEN
+                    J = LEN_TRIM( CMACT( S ) )
+                    IF( CMACT( S ) /= ' ' .AND.
+     &                  J > COLWID( M1 ) ) COLWID( M1 ) = J
+     
+                    J = LEN_TRIM( CNAICS( S ) )
+                    IF( CNAICS( S ) /= ' ' .AND.
+     &                  J > COLWID( M2 ) ) COLWID( M2 ) = J
+                        
+                    J = LEN_TRIM( CSRCTYP( S ) )
+                    IF( CSRCTYP( S ) /= ' ' .AND.
+     &                  J > COLWID( M3 ) ) COLWID( M3 ) = J
+                END IF
+            
             CASE( 'MOBILE' ) 
 
                 J = LEN_TRIM( CVTYPE( S ) )                   ! could be blank
@@ -302,10 +338,26 @@ C.........  Get the maximum column width for each of the columns in ASCII file
                 J = LEN_TRIM( CBLRID( S ) )                 ! could be blank
                 IF( CBLRID( S ) .NE. ' ' .AND.
      &              J .GT. COLWID( M3 ) ) COLWID( M3 ) = J
+     
+                J = LEN_TRIM( CMACT( S ) )                 ! could be blank
+                IF( CMACT( S ) .NE. ' ' .AND.
+     &              J .GT. COLWID( M4 ) ) COLWID( M4 ) = J
+     
+                J = LEN_TRIM( CNAICS( S ) )                 ! could be blank
+                IF( CNAICS( S ) .NE. ' ' .AND.
+     &              J .GT. COLWID( M5 ) ) COLWID( M5 ) = J
+     
+                J = LEN_TRIM( CSRCTYP( S ) )                 ! could be blank
+                IF( CSRCTYP( S ) .NE. ' ' .AND.
+     &              J .GT. COLWID( M6 ) ) COLWID( M6 ) = J
+     
+                J = LEN_TRIM( CERPTYP( S ) )                 ! could be blank
+                IF( CERPTYP( S ) .NE. ' ' .AND.
+     &              J .GT. COLWID( M7 ) ) COLWID( M7 ) = J
 
                 J = LEN_TRIM( CPDESC( S ) )                 ! could be blank
                 IF( CPDESC( S ) .NE. ' ' .AND.
-     &              J .GT. COLWID( M4 ) ) COLWID( M4 ) = J
+     &              J .GT. COLWID( M8 ) ) COLWID( M8 ) = J
 
             END SELECT
 
@@ -373,7 +425,24 @@ C.............  Split source characteristics into separate fields (CHARS)
      &                     NC, CHARS )
 
 C.............  Store remaining source attributes in separate fields (CHARS)
-            SELECT CASE ( CATEGORY )          
+            SELECT CASE ( CATEGORY )
+            CASE( 'AREA' )
+                IF( NONPOINT ) THEN
+                    IF( LF( M1 ) ) THEN
+                        NC = NC + 1
+                        CHARS( NC ) = CMACT( S )
+                    END IF
+                    
+                    IF( LF( M2 ) ) THEN
+                        NC = NC + 1
+                        CHARS( NC ) = CNAICS( S )
+                    END IF
+                    
+                    IF( LF( M3 ) ) THEN
+                        NC = NC + 1
+                        CHARS( NC ) = CSRCTYP( S )
+                    END IF
+                END IF          
             CASE( 'MOBILE' )
                 IF( LF( M1 ) ) THEN
                     NC = NC + 1
@@ -397,6 +466,26 @@ C.............  Store remaining source attributes in separate fields (CHARS)
                 END IF
 
                 IF( LF( M4 ) ) THEN
+                    NC = NC + 1
+                    CHARS( NC ) = CMACT( S )
+                END IF
+
+                IF( LF( M5 ) ) THEN
+                    NC = NC + 1
+                    CHARS( NC ) = CNAICS( S )
+                END IF
+
+                IF( LF( M6 ) ) THEN
+                    NC = NC + 1
+                    CHARS( NC ) = CSRCTYP( S )
+                END IF
+
+                IF( LF( M7 ) ) THEN
+                    NC = NC + 1
+                    CHARS( NC ) = CERPTYP( S )
+                END IF
+
+                IF( LF( M8 ) ) THEN
                     NC = NC + 1
                     CHARS( NC ) = CPDESC( S )
                 END IF
