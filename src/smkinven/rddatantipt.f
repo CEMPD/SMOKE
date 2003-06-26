517e517ee0441627dc3826dae36d42ab88702798
Catherine Seppanen <cseppan@email.unc.edu>
2003-06-26 15:02:07 +0000

    Added new fields from file format, adjusted other columns as needed

--- src/smkinven/rddatantipt.f
+++ src/smkinven/rddatantipt.f
@@ -1,7 +1,8 @@
 
-        SUBROUTINE RDDATANTIPT( LINE, READDATA, READPOL, IYEAR, 
-     &                          DESC, HT, DM, TK, FL, VL, SIC, 
-     &                          MACT, NAICS, LAT, LON, HDRFLAG, EFLAG )
+        SUBROUTINE RDDATANTIPT( LINE, READDATA, READPOL, IYEAR, DESC,
+     &                          ERPTYP, SRCTYP, HT, DM, TK, FL, VL, SIC, 
+     &                          MACT, NAICS, CTYPE, LAT, LON, UTMZ, 
+     &                          HDRFLAG, EFLAG )
 
 C***********************************************************************
 C  subroutine body starts at line 156
@@ -42,7 +43,7 @@ C***************************************************************************
 
 C...........   MODULES for public variables
 C.........  This module contains the information about the source category
-        USE MODINFO, ONLY: NEM, NOZ, NEF, NCE, NRE, NC1, NC2
+        USE MODINFO, ONLY: NEM, NDY, NEF, NCE, NRE, NC1, NC2
 
         IMPLICIT NONE
 
@@ -61,6 +62,8 @@ C...........   SUBROUTINE ARGUMENTS
         CHARACTER(LEN=IOVLEN3), INTENT (OUT) :: READPOL( 1 )          ! array of pollutant names
         INTEGER,                INTENT (OUT) :: IYEAR                 ! inventory year
         CHARACTER(LEN=40),      INTENT (OUT) :: DESC                  ! plant description
+        CHARACTER(LEN=ERPLEN3), INTENT (OUT) :: ERPTYP                ! emissions release point type
+        CHARACTER(LEN=STPLEN3), INTENT (OUT) :: SRCTYP                ! source type code
         CHARACTER(LEN=4),       INTENT (OUT) :: HT                    ! stack height
         CHARACTER(LEN=6),       INTENT (OUT) :: DM                    ! stack diameter
         CHARACTER(LEN=4),       INTENT (OUT) :: TK                    ! exit temperature
@@ -69,14 +72,16 @@ C...........   SUBROUTINE ARGUMENTS
         CHARACTER(LEN=SICLEN3), INTENT (OUT) :: SIC                   ! SIC
         CHARACTER(LEN=MACLEN3), INTENT (OUT) :: MACT                  ! MACT code
         CHARACTER(LEN=NAILEN3), INTENT (OUT) :: NAICS                 ! NAICS code
+        CHARACTER(LEN=1),       INTENT (OUT) :: CTYPE                 ! coordinate type
         CHARACTER(LEN=9),       INTENT (OUT) :: LAT                   ! stack latitude
         CHARACTER(LEN=9),       INTENT (OUT) :: LON                   ! stack longitude
+        CHARACTER(LEN=2),       INTENT (OUT) :: UTMZ                  ! UTM zone
         LOGICAL,                INTENT (OUT) :: HDRFLAG               ! true: line is a header line
         LOGICAL,                INTENT (OUT) :: EFLAG                 ! error flag
 
 C...........   Local parameters, indpendent
         INTEGER, PARAMETER :: MXPOLFIL = 60  ! arbitrary maximum pollutants in file
-        INTEGER, PARAMETER :: NSEG = 25      ! number of segments in line
+        INTEGER, PARAMETER :: NSEG = 28      ! number of segments in line
 
 C...........   Other local variables
         INTEGER         I       ! counters and indices
@@ -88,7 +93,7 @@ C...........   Other local variables
 
         LOGICAL, SAVE:: FIRSTIME = .TRUE. ! true: first time routine is called
  
-        CHARACTER(LEN=25)      SEGMENT( NSEG ) ! segments of line
+        CHARACTER(LEN=40)      SEGMENT( NSEG ) ! segments of line
         CHARACTER(LEN=CASLEN3) TCAS            ! tmp cas number
         CHARACTER(LEN=300)     MESG            ! message buffer
 
@@ -129,26 +134,30 @@ C.........  Separate line into segments
 
 C.........  Use the file format definition to parse the line into
 C           the various data fields
-        DESC  = ADJUSTL( SEGMENT( 7 ) )   ! plant description        
-        HT    = SEGMENT( 9 )              ! stack height
-        DM    = SEGMENT( 10 )             ! stack diameter
-        TK    = SEGMENT( 11 )             ! exit temperature
-        FL    = SEGMENT( 12 )             ! flow rate
-        VL    = SEGMENT( 13 )             ! exit velocity
-        SIC   = SEGMENT( 14 )             ! SIC
-        MACT  = ADJUSTL( SEGMENT( 15 ) )  ! MACT code
-        NAICS = ADJUSTL( SEGMENT( 16 ) )  ! NAICS code
-        LAT   = SEGMENT( 17 )             ! stack latitude
-        LON   = SEGMENT( 18 )             ! stack longitude
+        DESC   = ADJUSTL( SEGMENT( 6 ) )   ! plant description
+        ERPTYP = ADJUSTL( SEGMENT( 8 ) )   ! emissions release point type 
+        SRCTYP = ADJUSTL( SEGMENT( 9 ) )   ! source type code    
+        HT     = SEGMENT( 10 )             ! stack height
+        DM     = SEGMENT( 11 )             ! stack diameter
+        TK     = SEGMENT( 12 )             ! exit temperature
+        FL     = SEGMENT( 13 )             ! flow rate
+        VL     = SEGMENT( 14 )             ! exit velocity
+        SIC    = SEGMENT( 15 )             ! SIC
+        MACT   = ADJUSTL( SEGMENT( 16 ) )  ! MACT code
+        NAICS  = ADJUSTL( SEGMENT( 17 ) )  ! NAICS code
+        CTYPE  = ADJUSTL( SEGMENT( 18 ) )  ! coordinate type
+        LAT    = SEGMENT( 19 )             ! stack latitude
+        LON    = SEGMENT( 20 )             ! stack longitude
+        UTMZ   = ADJUSTL( SEGMENT( 21 ) )  ! UTM zone
 
-        READPOL ( 1     ) = SEGMENT( 19 )
-        READDATA( 1,NEM ) = SEGMENT( 20 ) ! annual emissions
-        READDATA( 1,NOZ ) = SEGMENT( 21 ) ! average-day emissions
+        READPOL ( 1     ) = SEGMENT( 22 )
+        READDATA( 1,NEM ) = SEGMENT( 23 ) ! annual emissions
+        READDATA( 1,NDY ) = SEGMENT( 24 ) ! average-day emissions
         READDATA( 1,NEF ) = ' '           ! emission factor
-        READDATA( 1,NCE ) = SEGMENT( 22 ) ! control efficiency
-        READDATA( 1,NRE ) = SEGMENT( 23 ) ! rule effectiveness
-        READDATA( 1,NC1 ) = SEGMENT( 24 ) ! primary control equipment code
-        READDATA( 1,NC2 ) = SEGMENT( 25 ) ! secondary control equipment code
+        READDATA( 1,NCE ) = SEGMENT( 25 ) ! control efficiency
+        READDATA( 1,NRE ) = SEGMENT( 26 ) ! rule effectiveness
+        READDATA( 1,NC1 ) = SEGMENT( 27 ) ! primary control equipment code
+        READDATA( 1,NC2 ) = SEGMENT( 28 ) ! secondary control equipment code
             
 C.........  Make sure routine knows it's been called already
         FIRSTIME = .FALSE.
