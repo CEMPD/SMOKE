9d69330db2cdd9f343b592bde1f7f7bc5c230c2f
Catherine Seppanen <cseppan@email.unc.edu>
2004-02-16 20:06:45 +0000

    Modified relative humidity structures; now stores hourly mixing ratios by county from Premobl and computes relative humidity in Emisfac

--- src/emmod/modmet.f
+++ src/emmod/modmet.f
@@ -51,27 +51,31 @@
 !...........   Hourly meteorology data
 !...              for Mobile5 processing, index 0 = 12 AM local time
 !...              for Mobile6 processing, index 0 = 6 AM local time
-        REAL,    ALLOCATABLE, PUBLIC :: TKHOUR  ( :,: ) ! temps by source per hour
-        REAL,    ALLOCATABLE, PUBLIC :: RHHOUR  ( :,: ) ! relative humidity by source per hour
-        REAL,    ALLOCATABLE, PUBLIC :: BPHOUR  ( :,: ) ! barometric pressure by source per hour
+        REAL,    ALLOCATABLE, PUBLIC :: TKHOUR  ( :,: ) ! temps by source per hour (Premobl)
+                                                        ! temps by county per hour (Emisfac)
+        REAL,    ALLOCATABLE, PUBLIC :: QVHOUR  ( :,: ) ! mixing ratios by source per hour (Premobl)
+                                                        ! mixing ratios by county per hour (Emisfac)
+        REAL,    ALLOCATABLE, PUBLIC :: BPHOUR  ( :,: ) ! barometric pressure by source (Premobl)
+                                                        ! barometric pressure by county (Emisfac)
+        REAL,    ALLOCATABLE, PUBLIC :: RHHOUR  ( :,: ) ! relative humidity by county per hour
 
         REAL,    ALLOCATABLE, PUBLIC :: TDYCNTY ( : )   ! daily temps by county
-        REAL,    ALLOCATABLE, PUBLIC :: RHDYCNTY( : )   ! daily relative humidity by county
+        REAL,    ALLOCATABLE, PUBLIC :: QVDYCNTY( : )   ! daily mixing ratios by county
         REAL,    ALLOCATABLE, PUBLIC :: BPDYCNTY( : )   ! daily barometric pressure by county
         INTEGER, ALLOCATABLE, PUBLIC :: DYCODES ( : )   ! FIPS codes for daily counties
 
         REAL,    ALLOCATABLE, PUBLIC :: TWKCNTY ( : )   ! weekly temps by county
-        REAL,    ALLOCATABLE, PUBLIC :: RHWKCNTY( : )   ! weekly relative humidity by county
+        REAL,    ALLOCATABLE, PUBLIC :: QVWKCNTY( : )   ! weekly mixing ratios by county
         REAL,    ALLOCATABLE, PUBLIC :: BPWKCNTY( : )   ! weekly barometric pressure by county
         INTEGER, ALLOCATABLE, PUBLIC :: WKCODES ( : )   ! FIPS codes for weekly counties
 
         REAL,    ALLOCATABLE, PUBLIC :: TMNCNTY ( : )   ! monthly temps by county
-        REAL,    ALLOCATABLE, PUBLIC :: RHMNCNTY( : )   ! monthly relative humidity by county
+        REAL,    ALLOCATABLE, PUBLIC :: QVMNCNTY( : )   ! monthly mixing ratios by county
         REAL,    ALLOCATABLE, PUBLIC :: BPMNCNTY( : )   ! monthly barometric pressure by county
         INTEGER, ALLOCATABLE, PUBLIC :: MNCODES ( : )   ! FIPS codes for monthly counties
 
         REAL,    ALLOCATABLE, PUBLIC :: TEPCNTY ( : )   ! episode temps by county
-        REAL,    ALLOCATABLE, PUBLIC :: RHEPCNTY( : )   ! episode relative humidity by county
+        REAL,    ALLOCATABLE, PUBLIC :: QVEPCNTY( : )   ! episode mixing ratios by county
         REAL,    ALLOCATABLE, PUBLIC :: BPEPCNTY( : )   ! episode barometric pressure by county
         INTEGER, ALLOCATABLE, PUBLIC :: EPCODES ( : )   ! FIPS codes for episode counties
 
