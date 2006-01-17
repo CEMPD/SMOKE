02994b337c23163f22bbea1f1db4b4600d48692b
B.H. Baek <bbaek@email.unc.edu>
2006-01-17 17:23:58 +0000

    modified on reading surrogate. No more fixed SRGFIPS and NSRGFIPS. Works like grdrssrg.f (no check on total surrogate fractions)

--- src/grdmat/sizrdsrg.f
+++ src/grdmat/sizrdsrg.f
@@ -1,5 +1,5 @@
 
-        SUBROUTINE SIZRDSRG( NT, VFLAG ) 
+        SUBROUTINE SIZRDSRG( NT, TMPLINE, VFLAG ) 
 
 C***********************************************************************
 C  subroutine body starts at line 117
@@ -40,8 +40,8 @@ C...........   Modules for public variables
 C...........   This module contains the gridding surrogates tables
         USE MODSURG, ONLY: IDXSRGA, IDXSRGB, SCELLA, SFIPSA, SSRGIDA,
      &                     SFRACA, NSRGREC, NSRGFIPS, SRGFIPS, FIPCELL,
-     &                     NCELLS, SRGFRAC, SRGCSUM, TMPLINE, SRGFMT,
-     &                     SRGNCOLS, SRGNROWS, MXCFIP
+     &                     NCELLS, SRGFRAC, SRGCSUM, SRGFMT, SRGNCOLS,
+     &                     SRGNROWS, MXCFIP
 
 C.........  This module contains the global variables for the 3-d grid
         USE MODGRID, ONLY: XOFF, YOFF, NCOLS, NROWS
@@ -63,6 +63,7 @@ C...........   EXTERNAL FUNCTIONS and their descriptions:
 
 C...........   Subroutine arguments
         INTEGER      , INTENT  (IN) :: NT         ! no of surrogate entry
+        CHARACTER(*) , INTENT  (IN) :: TMPLINE( NT ) ! tmp line buffer
         LOGICAL      , INTENT  (IN) :: VFLAG      ! true: using variable grid
 
 C...........   Local parameters
@@ -86,14 +87,13 @@ C...........   Local variables
         INTEGER         LC, LR                ! length of COLRANGE & ROWRANGE
         INTEGER         LCEL                  ! cell ID from previous iteration
         INTEGER         LFIP                  ! county code from prev iteration
-        INTEGER         LSSC                  ! srg ID from previous iteration
         INTEGER         NSRGALL               ! No. entries in surrgoates file
         INTEGER         ROW                   ! Tmp grid row number (y-axis)
         INTEGER         SSC                   ! Tmp spatial surrogate code
+        INTEGER         LCC                   ! previous tmp cell ID
 
-        LOGICAL, SAVE :: FIRSTIME = .TRUE.
-
-        REAL            RATIO                 ! Temp spatial surrogate Ratio
+        REAL            RATIO                 ! Tmp spatial surrogate Ratio
+        REAL            LRATIO                ! previous tmp spatial surrogate Ratio
 
         LOGICAL      :: GFLAG = .FALSE.       ! true: per iteration surg > 1
         LOGICAL      :: HFLAG = .FALSE.       ! true: header found
@@ -104,7 +104,7 @@ C...........   Local variables
         CHARACTER(20)   COLRANGE              ! buffer w/ column range
         CHARACTER(20)   ROWRANGE              ! buffer w/ row range
         CHARACTER(80)   LINE                  ! Read buffer for a line
-        CHARACTER(600)  MESG                  ! Message buffer
+        CHARACTER(300)  MESG                  ! Message buffer
 
         CHARACTER(16) :: PROGNAME = 'SIZRDSRG'    !  program name
 
@@ -183,7 +183,7 @@ C.................  Check the value of the column number
                     OFLAG = .TRUE.
                     WRITE( MESG,94010 ) 'WARNING: Column value ', COL,
      &                     'is outside range ' // COLRANGE( 1:LC ) // 
-     &                     ' at line', IREC
+     &                     ' from FIPS ', FIP, 'and surrogate', SSC
                     CALL M3MESG( MESG )
                 END IF
 
@@ -194,7 +194,7 @@ C.................  Check the value of the row number
                     OFLAG = .TRUE.
                     WRITE( MESG,94010 ) 'WARNING: Row value ', ROW,
      &                     'is outside range ' // ROWRANGE( 1:LR ) // 
-     &                     ' at line', IREC
+     &                     ' from FIPS ', FIP, 'and surrogate', SSC
                     CALL M3MESG( MESG )                    
 
 C.................  Special treatment for cell (0,0) (skip for now)
@@ -215,9 +215,10 @@ C.................  Check the value of the ratio value
                 IF( RATIO .GT. 1. ) THEN
                     WRITE( MESG,94020 )
      &                     'WARNING: resetting surrogates ratio at ' //
-     &                     'line', IREC, 'from', RATIO, 'to 1.'
-     &                     
+     &                     ' from FIPS ', FIP, 'and surrogate', SSC,
+     &                     ' from', RATIO, 'to 1.'
                     CALL M3MESG( MESG )
+
                     RATIO = 1.
                 END IF
                 
@@ -232,7 +233,7 @@ C.................  Skip entry if rows and columns are out of range
                 SFIPSA ( J ) = FIP
                 SSRGIDA( J ) = SSC
                 SFRACA ( J ) = RATIO
-
+                
             END DO
             
             CONTINUE
@@ -258,221 +259,126 @@ C.........  Now create the derived surrogates tables from the original data...
 C.........  Sort surrogates by county code & cell & surrogate code
         CALL SORTI3( NSRGALL, IDXSRGA, SFIPSA, SCELLA, SSRGIDA )
 
-C.........  Sort surrogates by surrogate code
-        CALL SORTI1( NSRGALL, IDXSRGB, SSRGIDA )
-        
-        IF( FIRSTIME ) THEN
-
-C.............  Count county codes in surrogates file and maximum number of cells
-C           per cy/st/co code.
-            LFIP     = -1
-            LCEL     = -1
-            MXCFIP   = 0
-            NSRGFIPS = 0
-            CELCNT   = 0
-            DO I = 1, NSRGALL
-           
-                J   = IDXSRGA( I )
-                FIP = SFIPSA ( J )
-                C   = SCELLA ( J )
-           
-                IF( FIP .NE. LFIP ) THEN
-           
-                    IF( CELCNT .GT. MXCFIP ) MXCFIP = CELCNT
-           
-                    NSRGFIPS = NSRGFIPS + 1  ! incrmt cntr for county in srg file
-                    CELCNT   = 0             ! init cell counter per county
-                    LFIP     = FIP
-           
-               END IF
-           
-                IF( C   .NE. LCEL .OR. CELCNT .EQ. 0 ) THEN
-                    CELCNT = CELCNT + 1      ! increment cell counter per county
-                    LCEL = C
-                END IF
-           
-            END DO
-           
-            IF( CELCNT .GT. MXCFIP ) MXCFIP = CELCNT
-           
-            K = 1            ! single srg code per run
-           
-C.............  Allocate memory for derived surrogates tables
-            ALLOCATE( NCELLS( NSRGFIPS ), STAT=IOS )
-            CALL CHECKMEM( IOS, 'NCELLS', PROGNAME )
-           
-            ALLOCATE( SRGFIPS( NSRGFIPS ), STAT=IOS )
-            CALL CHECKMEM( IOS, 'SRGFIPS', PROGNAME )
-           
-            ALLOCATE( FIPCELL( MXCFIP, NSRGFIPS ), STAT=IOS )
-            CALL CHECKMEM( IOS, 'FIPCELL', PROGNAME )
-            
-            ALLOCATE( SRGFRAC( K, MXCFIP, NSRGFIPS ), STAT=IOS )
-            CALL CHECKMEM( IOS, 'SRGFRAC', PROGNAME )
-           
-            ALLOCATE( SRGCSUM( K, NSRGFIPS ), STAT=IOS )
-            CALL CHECKMEM( IOS, 'SRGCSUM', PROGNAME )
-           
-C.............  Initialize arrays
-            NCELLS  = 0  ! array
-            SRGFIPS = 0  ! array
-            FIPCELL = 0  ! array
-            SRGFRAC = 0. ! array
-            SRGCSUM = 0. ! array
-           
-C.................  Store the surrogate fractions, FIPS codes, and cell numbers...
-            LFIP     = -1
-            LCEL     = -1
-            NSRGFIPS = 0
-            DO I = 1, NSRGALL
-              
-                J     = IDXSRGA( I )
-                FIP   = SFIPSA ( J )
-                C     = SCELLA ( J )
-                SSC   = SSRGIDA( J )
-                RATIO = SFRACA ( J )
-           
-                IF( FIP .NE. LFIP ) THEN
-           
-                    NSRGFIPS = NSRGFIPS + 1  ! incrmt cntr for county in srg file
-                    SRGFIPS( NSRGFIPS ) = FIP
-                    CELCNT   = 0             ! init cell counter per county
-                    LFIP     = FIP
-                  
-                END IF
-           
-                IF( C   .NE. LCEL .OR. CELCNT .EQ. 0 ) THEN
-           
-                    CELCNT = CELCNT + 1      ! increment cell counter per county
-                    LCEL = C
-           
-                END IF
-           
-C.................  Store cell count at every iteration, and it will get 
-C                   overwritten until the maximum value for each county is stored
-                NCELLS ( NSRGFIPS ) = CELCNT  
-           
-C.................  Store cell number  at every iteration, but it will be the same
-C                   each time
-                FIPCELL( CELCNT, NSRGFIPS ) = C  
-           
-                SRGFRAC( K, CELCNT, NSRGFIPS ) = RATIO
-                SRGCSUM( K, NSRGFIPS ) = SRGCSUM( K, NSRGFIPS ) + RATIO
-           
-            END DO
-            
-            FIRSTIME = .FALSE.
-        
-        ELSE
-
-C.............  Count county codes in surrogates file and maximum number of cells
-C           per cy/st/co code.
-            FIPCELL = 0  ! array
-            SRGFRAC = 0. ! array
-            SRGCSUM = 0. ! array
-            K = 1            ! single srg code per run
-           
-C.............  Allocate memory for derived surrogates tables
-            ALLOCATE( FIPCELL( MXCFIP, NSRGFIPS ), STAT=IOS )
-            CALL CHECKMEM( IOS, 'FIPCELL', PROGNAME )
-            
-            ALLOCATE( SRGFRAC( K, MXCFIP, NSRGFIPS ), STAT=IOS )
-            CALL CHECKMEM( IOS, 'SRGFRAC', PROGNAME )
-           
-            ALLOCATE( SRGCSUM( K, NSRGFIPS ), STAT=IOS )
-            CALL CHECKMEM( IOS, 'SRGCSUM', PROGNAME )
-           
-C.............  Initialize arrays
-            FIPCELL = 0  ! array
-            SRGFRAC = 0. ! array
-            SRGCSUM = 0. ! array
-           
-C.................  Store the surrogate fractions, FIPS codes, and cell numbers...
-            LFIP     = -1
-            LCEL     = -1
-            DO I = 1, NSRGALL
-              
-                J     = IDXSRGA( I )
-                FIP   = SFIPSA ( J )
-                C     = SCELLA ( J )
-                SSC   = SSRGIDA( J )
-                RATIO = SFRACA ( J )
-           
-                IF( FIP .NE. LFIP ) THEN
-           
-                    F      = FIND1( FIP, NSRGFIPS, SRGFIPS )
-                    CELCNT = NCELLS( F )
-                    LFIP   = FIP
-                  
-                END IF
-           
-C.................  Store cell count at every iteration, and it will get 
-C                   overwritten until the maximum value for each county is stored
-                DO J = 1, CELCNT
-
-C.................  Store cell number  at every iteration, but it will be the same
-C                   each time
-                    FIPCELL( J, F ) = C  
-           
-                    SRGFRAC( K, J, F ) = RATIO
-                    SRGCSUM( K, F ) = SRGCSUM( K, F ) + RATIO
-                
-                END DO
-           
-            END DO
-            
-       END IF
+C.........  Count county codes in surrogates file and maximum number of cells
+C       per cy/st/co code.
+        LFIP     = -1
+        LCEL     = -1
+        MXCFIP   = 0
+        NSRGFIPS = 0
+        CELCNT   = 0
+        DO I = 1, NSRGALL
       
-C.........  Now check to make sure the surrogate totals for each county are 
-C           less than or equal to 1.
+            J   = IDXSRGA( I )
+            FIP = SFIPSA ( J )
+            C   = SCELLA ( J )
       
-        DO I = 1, NSRGFIPS
+            IF( FIP .NE. LFIP ) THEN
       
-            GFLAG = .FALSE.
-            CNTCHK = 0
+                IF( CELCNT .GT. MXCFIP ) MXCFIP = CELCNT
       
-C.........  Check if county total surrogates greater than 1
-            IF( SRGCSUM( K,I ) .GT. 1.001 ) THEN
-      
-C.........  If first problem on this line
-                IF( .NOT. GFLAG ) THEN
-                    WRITE( MESG,94030 ) 'WARNING: County ' //
-     &              'surrogate total greater than 1. for '//
-     &              'county' // CRLF() // BLANK10, SRGFIPS( I ), 
-     &              ', SSC(', K , '):', SRGCSUM( K,I )
-                    GFLAG = .TRUE.
-                    CNTCHK = CNTCHK + 1
-      
-C.............  If multiple problems on this line, but fewer than
-C               the MESG length will permit, add to message
-                ELSE IF( CNTCHK .LE. 27 ) THEN
-                    L = LEN_TRIM( MESG )
-                    WRITE( MESG,94031 ) MESG( 1:L )// ', SSC(', 
-     &                       K , '):', SRGCSUM( K,I )
-                    CNTCHK = CNTCHK + 1
-      
-                END IF
+                NSRGFIPS = NSRGFIPS + 1  ! incrmt cntr for county in srg file
+                CELCNT   = 0             ! init cell counter per county
+                LFIP     = FIP
       
            END IF
       
-C.............  Renormalize all surrogates with totals > 1
-            IF( SRGCSUM( K,I ) .GT. 1. ) THEN
-                RFLAG = .TRUE.  ! Set to give global warning
-                DO C = 1, NCELLS( I )
-                    SRGFRAC( K,C,I ) = SRGFRAC( K,C,I ) /
-     &                                 SRGCSUM( K,I )
-                END DO
+            IF( C   .NE. LCEL .OR. CELCNT .EQ. 0 ) THEN
+                CELCNT = CELCNT + 1      ! increment cell counter per county
+                LCEL = C
             END IF
       
-C.........  Give a warning message for significant counties
-            IF( GFLAG ) CALL M3MESG( MESG )
-
         END DO
       
-C.........  Reset number of surrogate records stored in the module with
-C           the correct number after reading file and removing records that
-C           are outside the subgrid (if any)
-        NSRGREC = NSRGALL
+        IF( CELCNT .GT. MXCFIP ) MXCFIP = CELCNT
+      
+        K = 1            ! single srg code per run
+  
+C.........  Allocate memory for derived surrogates tables
+        ALLOCATE( NCELLS( NSRGFIPS ), STAT=IOS )
+        CALL CHECKMEM( IOS, 'NCELLS', PROGNAME )
+      
+        ALLOCATE( SRGFIPS( NSRGFIPS ), STAT=IOS )
+        CALL CHECKMEM( IOS, 'SRGFIPS', PROGNAME )
+
+        ALLOCATE( FIPCELL( MXCFIP, NSRGFIPS ), STAT=IOS )
+        CALL CHECKMEM( IOS, 'FIPCELL', PROGNAME )
+        
+        ALLOCATE( SRGFRAC( K, MXCFIP, NSRGFIPS ), STAT=IOS )
+        CALL CHECKMEM( IOS, 'SRGFRAC', PROGNAME )
+    
+        ALLOCATE( SRGCSUM( K, NSRGFIPS ), STAT=IOS )
+        CALL CHECKMEM( IOS, 'SRGCSUM', PROGNAME )
+
+C.........  Initialize arrays
+        NCELLS  = 0  ! array
+        SRGFIPS = 0  ! array
+        FIPCELL = 0  ! array
+        SRGFRAC = 0. ! array
+        SRGCSUM = 0. ! array
+      
+C.............  Store the surrogate fractions, FIPS codes, and cell numbers...
+        LFIP     = -1
+        LCEL     = -1
+        NSRGFIPS = 0
+        LRATIO   = -1.
+
+        DO I = 1, NSRGALL
+          
+            J     = IDXSRGA( I )
+            FIP   = SFIPSA ( J )
+            C     = SCELLA ( J )
+            SSC   = SSRGIDA( J )
+            RATIO = SFRACA ( J )
+      
+            IF( FIP .NE. LFIP ) THEN
+      
+                NSRGFIPS = NSRGFIPS + 1  ! incrmt cntr for county in srg file
+                SRGFIPS( NSRGFIPS ) = FIP
+                CELCNT   = 0             ! init cell counter per county
+                LFIP     = FIP
+                LRATIO = RATIO
+                LCC    = C
+
+            ELSE
+                IF( LCC .EQ. C .AND. LRATIO .EQ. RATIO ) THEN
+                    WRITE( MESG,94011 ) 'WARNING: There are ' //
+     &                   'duplicaties entries with same FIPS', FIP,
+     &                   'surrogate ', SSC, 'and fraction value', RATIO
+                    CALL M3MESG( MESG )                    
+                END IF
+
+                IF( LCC .EQ. C .AND. LRATIO .NE. RATIO ) THEN
+                    WRITE( MESG,94012 ) 'ERROR : There are ' //
+     &                   'duplicaties entries with same FIPS', FIP,
+     &                   'and surrogate ', SSC, 'with different ' //
+     &                   'fraction values', RATIO, 'and ', LRATIO
+                    CALL M3EXIT( PROGNAME, 0, 0, MESG, 2 )                    
+                END IF
+
+                LCC    = C
+                LRATIO = RATIO
+
+            END IF
+
+            IF( C   .NE. LCEL .OR. CELCNT .EQ. 0 ) THEN
+      
+                CELCNT = CELCNT + 1      ! increment cell counter per county
+                LCEL = C
+      
+            END IF
+      
+C.............  Store cell count at every iteration, and it will get 
+C               overwritten until the maximum value for each county is stored
+            NCELLS ( NSRGFIPS ) = CELCNT  
+      
+C.............  Store cell number  at every iteration, but it will be the same
+C               each time
+            FIPCELL( CELCNT, NSRGFIPS ) = C  
+      
+            SRGFRAC( K, CELCNT, NSRGFIPS ) = RATIO
+            SRGCSUM( K, NSRGFIPS ) = SRGCSUM( K, NSRGFIPS ) + RATIO
+
+        END DO
 
 C.....  Deallocate local variables
 
@@ -490,6 +396,10 @@ C...........   Formatted file I/O formats............ 93xxx
 C...........   Internal buffering formats............ 94xxx
 
 94010   FORMAT( 10( A, :, I8, :, 1X ) )
+
+94011   FORMAT( A, I8, 1X, A, I5, 1X, A, F8.4 )
+
+94012   FORMAT( A, I8, 1X, A, I5, 1X, A, F8.4, A, F8.4 )
 
 94020   FORMAT( A, 1X, I8, 1X, A, 1X, F10.6, 1X, A )
 
