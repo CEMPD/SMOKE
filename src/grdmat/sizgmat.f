188c79005d22712151e4af8024ed323ae7f55d32
B.H. Baek <bbaek@email.unc.edu>
2006-03-17 18:39:21 +0000

    : ----------------------------------------------------------------------

--- src/grdmat/sizgmat.f
+++ src/grdmat/sizgmat.f
@@ -15,7 +15,6 @@ C
 C  SUBROUTINES AND FUNCTIONS CALLED:
 C
 C  REVISION  HISTORY:
-C      Created by B.H. Baek 02/06
 C
 C**************************************************************************
 C
