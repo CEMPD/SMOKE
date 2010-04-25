279a1bfd4bd75cc05733e3b4739770e312340ebc
Catherine Seppanen <cseppan@email.unc.edu>
2010-04-25 23:15:44 +0000

    Rewrote bldsrccell.f routine to use gridding matrix instead of ungridding matrix

--- src/movesmrg/bldsrccell.f
+++ src/movesmrg/bldsrccell.f
@@ -1,11 +1,11 @@
 
-        SUBROUTINE BLDSRCCELL( NSRC, NMAT, NU, IU, CU )
+        SUBROUTINE BLDSRCCELL( NSRC, NGRID, NMAT, NX, IX, CX )
 
 C***********************************************************************
 C  subroutine BLDSRCCELL body starts at line
 C
 C  DESCRIPTION:
-C      This subroutine uses the ungridding matrix to build a list of grid
+C      This subroutine uses the gridding matrix to build a list of grid
 C      cells and associated fractions for each source.
 C
 C  PRECONDITIONS REQUIRED:  
@@ -46,16 +46,19 @@ C.........  INCLUDES:
 
 C.........  SUBROUTINE ARGUMENTS
         INTEGER, INTENT(IN) :: NSRC        ! number of sources
+        INTEGER, INTENT(IN) :: NGRID       ! number of grid cells
         INTEGER, INTENT(IN) :: NMAT        ! dimension for matrixes
-        INTEGER, INTENT(IN) :: NU( NSRC )  ! number of cells per source
-        INTEGER, INTENT(IN) :: IU( NMAT )  ! list of cells per source
-        REAL,    INTENT(IN) :: CU( NMAT )  ! list of grid cell fractions per source
+        INTEGER, INTENT(IN) :: NX( NGRID ) ! number of sources per cell
+        INTEGER, INTENT(IN) :: IX( NMAT )  ! list of sources per cell
+        REAL,    INTENT(IN) :: CX( NMAT )  ! list of source fractions per cell
 
 C.........  LOCAL VARIABLES and their descriptions:
 
 C.........  Other local variables
-        INTEGER   INDX, NG, S        ! indexes and counters
+        INTEGER   INDX, NG, NS       ! indexes and counters
         INTEGER   MXNSRCCELLS        ! max. no. cells per source
+        INTEGER   CELL               ! cell number
+        INTEGER   SRC                ! source number
         INTEGER   IOS                ! error status
 
         CHARACTER(16) :: PROGNAME = 'BLDSRCCELL' ! program name
@@ -66,12 +69,23 @@ C   begin body of subroutine BLDSRCCELL
 C.........  Determine maximum number of cells per source
         ALLOCATE( NSRCCELLS( NSRC ), STAT=IOS )
         CALL CHECKMEM( IOS, 'NSRCCELLS', PROGNAME )
+        NSRCCELLS = 0   ! array
 
         MXNSRCCELLS = 0
-        DO S = 1, NSRC
-        
-            NSRCCELLS( S ) = NU( S )
-            IF( NU( S ) > MXNSRCCELLS ) MXNSRCCELLS = NU( S )
+        INDX = 0
+        DO NG = 1, NGRID
+
+C.............  Loop over sources for current grid cell
+            DO NS = 1, NX( NG )
+            
+                INDX = INDX + 1
+                SRC = IX( INDX )
+                NSRCCELLS( SRC ) = NSRCCELLS( SRC ) + 1
+                IF( NSRCCELLS( SRC ) > MXNSRCCELLS ) THEN
+                    MXNSRCCELLS = NSRCCELLS( SRC )
+                END IF
+                
+            END DO
         
         END DO
 
@@ -80,15 +94,22 @@ C.........  Store list of cells and fractions for each source
         CALL CHECKMEM( IOS, 'SRCCELLS', PROGNAME )
         ALLOCATE( SRCCELLFRACS( NSRC, MXNSRCCELLS ), STAT=IOS )
         CALL CHECKMEM( IOS, 'SRCCELLFRACS', PROGNAME )
+        SRCCELLS = 0     ! array
+        SRCCELLFRACS = 0 ! array
+        NSRCCELLS = 0    ! array
 
         INDX = 0
-        DO S = 1, NSRC
+        DO NG = 1, NGRID
         
-            DO NG = 1, NSRCCELLS( S )
+            DO NS = 1, NX( NG )
 
                 INDX = INDX + 1
-                SRCCELLS    ( S,NG ) = IU( INDX )
-                SRCCELLFRACS( S,NG ) = CU( INDX )
+                SRC = IX( INDX )
+                NSRCCELLS( SRC ) = NSRCCELLS( SRC ) + 1
+                
+                CELL = NSRCCELLS( SRC )
+                SRCCELLS    ( SRC, CELL ) = NG
+                SRCCELLFRACS( SRC, CELL ) = CX( INDX )
             
             END DO
         
