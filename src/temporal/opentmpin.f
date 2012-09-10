3c9f39cda2308d4dfafb13c0b66b2398ae1c1cfc
B.H. Baek <bbaek@email.unc.edu>
2012-09-10 19:37:40 +0000

    Brought back FILL_ANNUAL and AVEDAY_YN combination setting warning message

--- src/temporal/opentmpin.f
+++ src/temporal/opentmpin.f
@@ -119,7 +119,12 @@ C.........  Get environment variables that control program behavior
 C.........  Waring message for imcompatiblity of FILL_ANNUAL setting from Smkinven
 C.........  Set average day emissions flag (INVPIDX)
         OFLAG = ENVYN( 'SMK_AVEDAY_YN', MESG, .FALSE., IOS )
-        IF( OFLAG ) INVPIDX = 1
+        IF( OFLAG ) THEN
+            MESG = 'CRITICAL: If FILL_ANNUAL was set to Y in '//
+     &             'SMKINVEN run, ' // CRLF() // BLANK10 //
+     &             'SMK_AVEDAY_YN must be set to N in TEMPORAL run.'
+            CALL M3MSG2( MESG )
+        END IF
 
 C.........  Prompt for and open inventory file
         INAME = ENAME 
@@ -168,6 +173,8 @@ C.........  Store source-category-specific header information,
 C           including the inventory pollutants in the file (if any).  Note that 
 C           the I/O API head info is passed by include file and the
 C           results are stored in module MODINFO.
+C.........  Set average day emissions flag (INVPIDX)
+        IF( OFLAG ) INVPIDX = 1
         CALL GETSINFO( ENAME )
 
 C.........  Reset activity to pollutant to create hourly VMT without running EMISFAC
