71199331d147fa4906ac84bcc6544a7ff0f5953c
Catherine Seppanen <cseppan@email.unc.edu>
2016-09-15 00:53:55 -0400

    Include source's lat-lon coordinates in AERMOD report for all point sectors

--- src/lib/prclinrc.f
+++ src/lib/prclinrc.f
@@ -837,6 +837,8 @@ C.............  AERMOD support report
                     RPT_%BYSAT     = .TRUE.
                     RPT_%BYSUN     = .TRUE.
 
+                    RPT_%LATLON    = .TRUE.      ! include lat-lon coords in report
+
                     IF( SEGMENT( 3 ) .EQ. 'PTNONIPM' .OR.
      &                  SEGMENT( 3 ) .EQ. 'PTEGU'         ) THEN
 
@@ -845,7 +847,6 @@ C.............  AERMOD support report
                         RPT_%STKPARM   = .TRUE.   ! By fugitive parameters
                         RPT_%FUGPARM   = .TRUE.
                         RPT_%BYLATLON  = .TRUE.   ! By lat and long coordinates
-                        RPT_%LATLON    = .TRUE.
 
                         IF( SEGMENT( 4 ) .EQ. 'EMIS' ) THEN
                             TFLAG          = .TRUE.    ! read PTMP file
