5df8b708d98b6c08890f6db7d78c61d20c131419
BH Baek <bhbaek@gmail.com>
2018-07-23 11:19:34 -0400

    Update the size of naming buff from 200 to 512

--- src/temporal/opentmp.f
+++ src/temporal/opentmp.f
@@ -102,8 +102,8 @@ C...........   Other local variables
         CHARACTER(300)  MESG        ! message buffer 
 
         CHARACTER(NAMLEN3)  NAMBUF       ! [A|M|P]TMP file name buffer
-        CHARACTER(200)  NAMBUFT          ! [A|M|P]TMPNAME file name buffer
-        CHARACTER(200)  NAMBUFS          ! [A|M|P]TSUPNAME file name buffer
+        CHARACTER(512)  NAMBUFT          ! [A|M|P]TMPNAME file name buffer
+        CHARACTER(512)  NAMBUFS          ! [A|M|P]TSUPNAME file name buffer
         CHARACTER(IODLEN3)  IFDESC2, IFDESC3 ! fields 2 & 3 from PNTS FDESC
 
         CHARACTER(16) :: PROGNAME = 'OPENTMP' ! program name
